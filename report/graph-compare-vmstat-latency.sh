6359e64bac2e357807b7bc01b5b3d1beb5d2c3dc
Mel Gorman <mgorman@suse.de>
2011-07-15 09:50:42 +0100

    report: Add graph report script to plot vmstat update latencies
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ report/graph-compare-vmstat-latency.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+DIRNAME=`dirname $0`
+SCRIPTDIR=`cd "$DIRNAME" && pwd`
+PLOT=$SCRIPTDIR/plot
+. $SCRIPTDIR/../config
+. $SCRIPTDIR/common-cmdline-parser.sh
+
+TITLES=
+for SINGLE_KERNEL in $KERNEL; do
+	START=`head -1 $WORKINGDIR/tests-timestamp-$SINGLE_KERNEL | awk '{print $3}'`
+
+	ANY_TEST=`ls $WORKINGDIR/vmstat-$SINGLE_KERNEL-* | head -1 | awk -F - '{print $NF}'`
+	if [ ! -e $WORKINGDIR/vmstat-$SINGLE_KERNEL-$ANY_TEST ]; then
+		ANY_TEST=`ls $WORKINGDIR/vmstat-$SINGLE_KERNEL-* | head -1 | awk -F - '{print $(NF-1)"-"$NF}'`
+		if [ ! -e $WORKINGDIR/vmstat-$SINGLE_KERNEL-$ANY_TEST ]; then
+        		echo Cannot find records for $ANY_TEST in $WORKINGDIR/vmstat-$SINGLE_KERNEL-$ANY_TEST
+        		exit -1
+		fi
+	fi 
+
+	# Is timestamp information available?
+	head -1 vmstat-$SINGLE_KERNEL-$ANY_TEST | grep -- -- > /dev/null
+	if [ $? -eq 0 ]; then
+		TIMESTAMPS=yes
+		awk "{print (\$1-$START)\" \"\$3}" vmstat-$SINGLE_KERNEL-* > /tmp/vmstat-latency-$NAME-$SINGLE_KERNEL.data-unsorted
+	else
+		echo ERROR: Timestamp and latency information unavailable
+		exit
+	fi
+	sort -n /tmp/vmstat-latency-$NAME-$SINGLE_KERNEL.data-unsorted > /tmp/vmstat-latency-$NAME-$SINGLE_KERNEL.data
+	rm /tmp/vmstat-latency-$NAME-$SINGLE_KERNEL.data-unsorted
+	
+	if [ "$TITLES" != "" ]; then
+		TITLES=$TITLES,
+	fi
+	TITLES="$TITLES$SINGLE_KERNEL"
+done
+if [ "$FORCE_TITLES" != "" ]; then
+	TITLES=$FORCE_TITLES
+fi
+if [ "$ARCH" = "" ]; then
+	ARCH=$NAME
+fi
+
+PLOTS=
+for SINGLE_KERNEL in $KERNEL; do
+	PLOTS="$PLOTS /tmp/vmstat-latency-$NAME-$SINGLE_KERNEL.data"
+done
+
+$PLOT \
+	--timeplot \
+	--title "$ARCH VMStat Latency Update Comparison" \
+	--extra /tmp/$NAME-extra \
+	--format "postscript color" \
+	--ylabel "Update Latency (seconds)" \
+	--titles $TITLES \
+	--output $OUTPUTDIR/vmstat-latency-comparison-$NAME.ps \
+	$PLOTS
+echo Generated vmstat-latency-comparison-$NAME.ps
+
+$PLOT \
+	--timeplot \
+	--using "smooth bezier" \
+	--extra /tmp/$NAME-extra \
+	--title "$ARCH VMStat Latency Update Comparison" \
+	--format "postscript color" \
+	--ylabel "Update Latency (seconds)" \
+	--titles $TITLES \
+	--output $OUTPUTDIR/vmstat-latency-comparison-smooth-$NAME.ps \
+	$PLOTS
+echo Generated vmstat-latency-comparison-smooth-$NAME.ps
