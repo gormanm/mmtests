e00f1edd20bbe59226f2f4c076164e68300843e6
Mel Gorman <mgorman@suse.de>
2012-01-04 10:01:05 +0000

    monitors: Add monitors for CPU hotplug testing
    This patch adds two monitors that test CPU online/offline while running
    some arbitrary other load. The stress test does not sleep to increase
    the chances a race can be triggered.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ monitors/watch-cpuoffline.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Assumes sequential ordering of CPUs. This is not always true but
+# sufficient for the test in mind
+NUM_CPU=`ls -d /sys/devices/system/cpu/cpu[0-9]* | wc -l`
+MAX_CPU_INDEX=$((NUM_CPU-1))
+
+while [ 1 ]; do
+	OFFLINED=0
+	for CPU in `seq 1 $MAX_CPU_INDEX`; do
+		echo 0 > /sys/devices/system/cpu/cpu$CPU/online
+		STATUS=`cat /sys/devices/system/cpu/cpu$CPU/online`
+		if [ "$STATUS" = "0" ]; then
+			OFFLINED=$((OFFLINED+1))
+		fi
+	done
+
+	ONLINED=0
+	for CPU in `seq 1 $MAX_CPU_INDEX`; do
+		echo 1 > /sys/devices/system/cpu/cpu$CPU/online
+		STATUS=`cat /sys/devices/system/cpu/cpu$CPU/online`
+		if [ "$STATUS" = "1" ]; then
+			ONLINED=$((ONLINED+1))
+		fi
+	done
+
+	echo cpuhotplug $NUM_CPU $OFFLINED $ONLINED
+	sleep $MONITOR_UPDATE_FREQUENCY 2> /dev/null
+done
