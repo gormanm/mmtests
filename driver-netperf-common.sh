dd5d9e593a41f8ef07915011249f4a4ad35276b3
Mel Gorman <mgorman@suse.de>
2012-03-08 16:41:25 +0000

    netperf: Bind netperf test to first and second CPU
    For reproducibility, bind the server to CPU 0 and the client to CPU 1.
    This means that chances are they will be on the same socket but more
    reprodible than using any available CPU.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- driver-netperf-common.sh
+++ driver-netperf-common.sh
@@ -2,7 +2,11 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-netperf \
+	BIND_SWITCH=
+	if [ "$NETPERF_BINDING" != "" ]; then
+		BIND_SWITCH=--bind-$NETPERF_BINDING
+	fi
+	$SCRIPTDIR/shellpacks/shellpack-bench-netperf $BIND_SWITCH \
 		--$PROTOCOL-only \
 		--buffer-sizes $NETPERF_BUFFER_SIZES
 }
