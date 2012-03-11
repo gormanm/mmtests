3080da98fd66aba9303360998b557bc621f60647
Mel Gorman <mgorman@suse.de>
2012-03-11 22:00:26 +0000

    pft: Support testing with multiple page sizes and default to base
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- driver-pft.sh
+++ driver-pft.sh
@@ -2,5 +2,30 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
-	$SHELLPACK_INCLUDE/shellpack-bench-pft
+	LOGDIR_TOPLEVEL=$LOGDIR_RESULTS
+	for PAGESIZE in $PFT_PAGESIZES; do
+		unset USELARGE
+		unset USE_DYNAMIC_HUGEPAGES
+		case $PAGESIZE in
+		base)
+			disable_transhuge
+			;;
+		transhuge)
+			if [ "$TRANSHUGE_AVAILABLE" = "yes" ]; then
+				enable_transhuge
+			else
+				echo THP support unavailable for transhuge
+				continue
+			fi
+			;;
+		default)
+			reset_transhuge
+			;;
+		esac
+
+		export LOGDIR_RESULTS=$LOGDIR_TOPLEVEL/$PAGESIZE
+		mkdir -p $LOGDIR_RESULTS
+		$SHELLPACK_INCLUDE/shellpack-bench-pft
+	done
+	export LOGDIR_RESULTS=$LOGDIR_TOPLEVEL
 }
