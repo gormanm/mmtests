afc986cdcd4a53692ccaf5b90d88f9d07200e1d7
Mel Gorman <mgorman@suse.de>
2012-03-15 12:27:35 +0000

    watch-interactive-apps: Make more CPU efficient
    Would be better in another language entirely but this will do for now.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- monitors/watch-interactive-apps.sh
+++ monitors/watch-interactive-apps.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 if [ "$MONITOR_INTERACTIVE_APPS" = "" ]; then
-	MONITOR_INTERACTIVE_APPS="evolution akregator gnome-terminal xchat firefox konqueror"
+	MONITOR_INTERACTIVE_APPS="evolution akregator gnome-terminal xchat firefox konqueror mutt vi"
 fi
 if [ "$MONITOR_INTERACTIVE_APPS_STACKIGNORE" = "" ]; then
-	MONITOR_INTERACTIVE_APPS_STACKIGNORE="poll_schedule_timeout do_wait"
+	MONITOR_INTERACTIVE_APPS_STACKIGNORE="poll_schedule_timeout do_wait fget_light inotify_poll sys_poll do_signal_stop"
 fi
+SEARCH_STACKIGNORE=`echo $MONITOR_INTERACTIVE_APPS_STACKIGNORE | sed -e 's/ /|/g'`
 
 PIDS=$$
 refresh_pids() {
 	PIDS=$$
 	for NAME in $MONITOR_INTERACTIVE_APPS; do
-		PIDS="$PIDS `ps aux | grep $NAME | grep -v grep | awk '{print $2}'`"
+		PIDS="$PIDS `ps aux | grep " $NAME " | grep -v grep | awk '{print $2}'`"
 	done
 }
 
+
+CURRENT_TIME=`date +%s`
+SLEEP_INTERVAL=0.2
+NEXT_DATE_REFRESH=5
+NEXT_DATE_UPDATE=0
 NEXT_REFRESH=0
 
 while [ 1 ]; do
-	CURRENT_TIME=`date +%s`
+	if [ $NEXT_DATE_UPDATE -eq $NEXT_DATE_REFRESH ]; then
+		CURRENT_TIME=`date +%s`
+		NEXT_DATE_UPDATE=0
+	fi
+	NEXT_DATE_UPDATE=$((NEXT_DATE_UPDATE+1))
 	for PID in $PIDS; do
 		if [ ! -e /proc/$PID/stack -o $CURRENT_TIME -gt $NEXT_REFRESH ]; then
 			refresh_pids
@@ -26,22 +36,18 @@ while [ 1 ]; do
 			continue
 		fi
 
-		SEEN=
-		COUNT=
 		cat /proc/$PID/stack > /tmp/watch-$$-interactive.txt
-		for OMIT in $MONITOR_INTERACTIVE_APPS_STACKIGNORE; do
-			grep -q $OMIT /tmp/watch-$$-interactive.txt
-			if [ $? -ne 0 ]; then
-				SEEN=$((SEEN+1))
+		SEEN=`egrep "$SEARCH_STACKIGNORE" /tmp/watch-$$-interactive.txt | wc -l`
+		if [ $SEEN -eq 0 ]; then
+			head -1 /tmp/watch-$$-interactive.txt | grep -q 0x
+			if [ $? -eq 0 ]; then
+				echo time: $CURRENT_TIME
+				echo proc: `ps --no-header -p $PID`
+				cat /tmp/watch-$$-interactive.txt
 			fi
-			COUNT=$((COUNT+1))
-		done
-		if [ $COUNT -eq $SEEN ]; then
-			echo time: $CURRENT_TIME
-			echo proc: `ps --no-header -p $PID`
-			cat /tmp/watch-$$-interactive.txt
 		fi
 	done
-	rm -f /tmp/watch-$$-interactive.txt
-	sleep 0.2
+	sleep $SLEEP_INTERVAL
 done
+
+rm -f /tmp/watch-$$-interactive.txt
