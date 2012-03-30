97f17b928b3ecaafe53a8b7e3aaf534c5c64a61d
Mel Gorman <mgorman@suse.de>
2012-03-30 15:28:10 +0100

    stat: Add script to calculate quartiles
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- stat/lib/VMR/Stat.pm
+++ stat/lib/VMR/Stat.pm
@@ -9,7 +9,7 @@ use vars qw (@ISA @EXPORT);
 use strict;
 
 @ISA    = qw(Exporter);
-@EXPORT = qw(&calc_mean &calc_stddev &calc_confidence_interval_lower &calc_confidence_interval_upper);
+@EXPORT = qw(&calc_mean &calc_stddev &calc_quartiles &calc_confidence_interval_lower &calc_confidence_interval_upper);
 
 # Values taken from a standard normal table
 my %za = (
@@ -62,6 +62,24 @@ sub calc_stddev {
 	}
 
 	return sqrt($diff / $n);
+}
+
+sub calc_quartiles {
+	my @x = sort { $a <=> $b} @_;
+	my @quartiles;
+
+	$quartiles[0] = 0;
+	$quartiles[1] = $x[int($#x * 0.25 + 0.5)];
+	$quartiles[2] = $x[int($#x * 0.50 + 0.5)];
+	$quartiles[3] = $x[int($#x * 0.75 + 0.5)];
+	$quartiles[4] = $x[$#x];
+
+	chomp($quartiles[1]);
+	chomp($quartiles[2]);
+	chomp($quartiles[3]);
+	chomp($quartiles[4]);
+
+	return \@quartiles;
 }
 
 sub calc_confidence_interval {
