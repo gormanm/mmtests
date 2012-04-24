5ab7e5e688781bcd54b6e95ade281e9159bff103
Mel Gorman <mgorman@suse.de>
2012-04-24 22:17:14 +0100

    parallelio: Allow the specification of a minimum IO size
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- driver-parallelio.sh
+++ driver-parallelio.sh
@@ -5,6 +5,7 @@ run_bench() {
 	rmdir $LOGDIR_RESULTS
 	$SCRIPTDIR/shellpacks/shellpack-bench-parallelio \
 		--io-load     $PARALLELIO_IOLOAD \
+		--min-io-size $PARALLELIO_MIN_IOSIZE \
 		--max-io-size $PARALLELIO_MAX_IOSIZE \
 		--workloads "$PARALLELIO_WORKLOADS" \
 		--workload-duration $PARALLELIO_WORKLOAD_DURATION
