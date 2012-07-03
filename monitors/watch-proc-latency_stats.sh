8d38d068cbce82eb5ff74ee95bb4a792348fc5c7
Mel Gorman <mgorman@suse.de>
2012-07-03 09:41:23 +0100

    monitors: Add monitor for /proc/latency_stats
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ monitors/watch-proc-latency_stats.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Handle being shutdown
+EXITING=0
+shutdown_read() {
+	echo 0 > /proc/sys/kernel/latencytop
+	EXITING=1
+	exit 0
+}
+
+trap shutdown_read TERM
+trap shutdown_read INT
+
+echo 1 > /proc/sys/kernel/latencytop
+echo 0 > /proc/latency_stats
+
+while [ 1 ]; do
+        # Check if we should shutdown 
+        if [ $EXITING -eq 1 ]; then 
+                exit 0 
+        fi 
+
+	echo
+	echo time: `date +%s`
+	sort -n -k2 /proc/latency_stats
+	sleep $MONITOR_UPDATE_FREQUENCY
+done
