11b73941b1faad6358f83a6b6d0d6ac7c0338f66
Mel Gorman <mgorman@suse.de>
2012-09-14 12:32:35 +0100

    nas: Add option to run with CMA
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- drivers/driver-nas-mpi.sh
+++ drivers/driver-nas-mpi.sh
@@ -2,6 +2,10 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-nas --type MPI
+	CMAOPT=
+	if [ "$NAS_USE_CMA" = "yes" ]; then 
+		CMAOPT="--cma"
+	fi
+	$SCRIPTDIR/shellpacks/shellpack-bench-nas --type MPI $CMAOPT
 	return $?
 }
