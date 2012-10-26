300687a27a34737d3b31b61596267a0e0359e8d2
Mel Gorman <mgorman@suse.de>
2012-10-26 10:32:30 +0100

    monitors: Add monitor for perf events
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ monitors/watch-perf-event-stat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$MONITOR_PERF_EVENTS" = "" ]; then
+	echo ERROR: Did not specify MONITOR_PERF_EVENTS in log
+	exit -1
+fi
+
+# Run perf sched system-wide 
+exec perf stat -a -e $MONITOR_PERF_EVENTS -o $MONITOR_LOG \
+	perl -e "open(OUTPUT, \">$MONITOR_PID\"); print OUTPUT \$\$; close OUTPUT; sleep;"
