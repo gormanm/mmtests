c61854dc7aeb2a2024ec4b9d3e24029bbba20a5c
Mel Gorman <mgorman@suse.de>
2012-11-03 18:29:21 +0000

    common-detect: Correct detection
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- shellpacks/common-detect.sh
+++ shellpacks/common-detect.sh
@@ -11,18 +11,19 @@ cpuinfo_val() {
 detect_mconf() {
 	# Common to all arches
 	# Lookup primary cache information
-	cache=/sys/devices/system/cpu/cpu0/cache
-	pcache=
+	export cache=/sys/devices/system/cpu/cpu0/cache
+	export pcache=
 	for index in `ls /sys/devices/system/cpu/cpu0/cache`; do
 		if [ "$pcache" != "" ]; then
 			pcache="$pcache + "
 		fi
-		pcache="$pcache`cat $cache/$index/size`"
-		pcache="$pcache `cat $cache/$index/type | head -c1`"
+		export pcache="$pcache`cat $cache/$index/size`"
+		export pcache="$pcache `cat $cache/$index/type | head -c1`"
 	done
-	hw_memory=`free -m | grep ^Mem: | awk '{print $2}'`MB
-	hw_cpus=`grep processor /proc/cpuinfo | wc -l`
+	export hw_memory=`free -m | grep ^Mem: | awk '{print $2}'`MB
+	export hw_cpus=`grep processor /proc/cpuinfo | wc -l`
 
+	ARCH=`uname -m`
 	case "$ARCH" in
 		i?86|x86_64|ia64)
 
@@ -30,29 +31,29 @@ detect_mconf() {
 				warning dmidecode is not in path, very limited info
 			fi
 
-			hw_manu=`dmidecode -s baseboard-manufacturer`
-			hw_prod=`dmidecode -s baseboard-product-name`
-			hw_vers=`dmidecode -s baseboard-version`
+			export hw_manu=`dmidecode -s baseboard-manufacturer`
+			export hw_prod=`dmidecode -s baseboard-product-name`
+			export hw_vers=`dmidecode -s baseboard-version`
 
-			hw_model="$hw_manu $hw_prod $hw_vers"
-			hw_cpu_name=`cpuinfo_val "model name"`
-			hw_cpu_mhz=`cpuinfo_val "cpu MHz"`
-			hw_ncoresperchip=`cpuinfo_val "cpu cores"`
-			hw_siblings=`cpuinfo_val siblings`
-			hw_nchips=$(($hw_cpus/$hw_ncoresperchip))
-			hw_ncores=$(($hw_cpus/$hw_nchips))
-			hw_pcache=$pcache
+			export hw_model="$hw_manu $hw_prod $hw_vers"
+			export hw_cpu_name=`cpuinfo_val "model name"`
+			export hw_cpu_mhz=`cpuinfo_val "cpu MHz"`
+			export hw_ncoresperchip=`cpuinfo_val "cpu cores"`
+			export hw_siblings=`cpuinfo_val siblings`
+			export hw_nchips=$(($hw_cpus/$hw_ncoresperchip))
+			export hw_ncores=$(($hw_cpus/$hw_nchips))
+			export hw_pcache=$pcache
 
-			hw_ht_enabled=Yes
-			if [ $hw_siblings -eq $hw_nw_ncoresperchip ]; then
-				hw_ht_enabled=No
+			export hw_ht_enabled=Yes
+			if [ $hw_siblings -eq $hw_ncoresperchip ]; then
+				export hw_ht_enabled=No
 			fi
 
 			;;
 		ppc64)
-			hw_cpu_name=`cpuinfo_val cpu`
-			hw_cpu_mhz=`cpuinfo_val "clock"`
-			hw_pcache=$pcache
+			export hw_cpu_name=`cpuinfo_val cpu`
+			export hw_cpu_mhz=`cpuinfo_val "clock"`
+			export hw_pcache=$pcache
 			;;
 	esac
 }
