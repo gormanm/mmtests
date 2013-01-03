bb8ec0d8a00992fefb4186b936e9bb89b6e1cc43
Mel Gorman <mgorman@suse.de>
2013-01-03 08:53:48 +0000

    ltp: Allow mmap10 test to be run directly
    Needed to reproduce a bug in 3.8-rc1
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- drivers/driver-ltp.sh
+++ drivers/driver-ltp.sh
@@ -2,7 +2,16 @@ FINEGRAINED_SUPPORTED=no
 NAMEEXTRA=
 
 run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-ltp \
-		--ltp-tests $LTP_RUN_TESTS
+	LTP_RUN_ARGS_SWITCH=
+	if [ "$LTP_RUN_ARGS" != "" ]; then
+		LTP_RUN_ARGS_SWITCH="--ltp-args \"$LTP_RUN_ARGS\""
+	fi
+	LTP_RUN_ITERATIONS_SWITCH=
+	if [ "$LTP_RUN_ITERATIONS" != "" ]; then
+		LTP_RUN_ITERATIONS_SWITCH="--ltp-iterations \"$LTP_RUN_ITERATIONS\""
+	fi
+
+	eval $SCRIPTDIR/shellpacks/shellpack-bench-ltp \
+			--ltp-tests "$LTP_RUN_TESTS" $LTP_RUN_ARGS_SWITCH $LTP_RUN_ITERATIONS_SWITCH
 	return $?
 }
