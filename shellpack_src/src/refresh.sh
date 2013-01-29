aa29d3f045a2bc88417c8313055182ed20cb5ba2
Zi Shen Lim <zlim.lnx@gmail.com>
2013-01-29 11:57:49 -0800

    refresh.sh: create output directory if not already there
    Signed-off-by: Zi Shen Lim <zlim.lnx@gmail.com>

--- shellpack_src/src/refresh.sh
+++ shellpack_src/src/refresh.sh
@@ -10,6 +10,10 @@ if [ ! -d $1 ]; then
 	exit -1
 fi
 
+if [ ! -d ../packs ]; then
+	mkdir ../packs
+fi
+
 ../bin/build-shellpack -s $1 -b $1-bench -i $1-install -o ../packs/shellpack-$1.tar.gz || exit -1
 ../bin/install-shellpack -p ../packs/shellpack-$1.tar.gz -i ../../shellpacks/ || exit -1
 
