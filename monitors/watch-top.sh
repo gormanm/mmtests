4781e2b6c65d540d11acf872853c5a37642650b9
Mel Gorman <mgorman@suse.de>
2013-03-16 15:08:47 +0000

    watch-top: Stop doubling up output
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- monitors/watch-top.sh
+++ monitors/watch-top.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-while [ 1 ]; do
-	echo time: `date +%s`
-	exec top -b -d $MONITOR_UPDATE_FREQUENCY -n 2 | perl -e 'while (<>) {
-		if ($_ =~ /^top -.*/) {
-			print "time: " . time . "\n$_";
-		}
-		print $_;}'
-done
+exec top -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
+	if ($_ =~ /^top -.*/) {
+		print "time: " . time . "\n";
+	}
+	print $_;}'
