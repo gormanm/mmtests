0982f9cf0b3a941cc02fc6b4f67b83a2243c6207
Mel Gorman <mgorman@suse.de>
2013-03-20 13:49:08 +0000

    reaim: Allow signal_test to be excludede from a workfile
    It's almost completely pointless.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- drivers/driver-reaim.sh
+++ drivers/driver-reaim.sh
@@ -2,14 +2,17 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
+	if [ "$REAIM_EXCLUDE" = "" ]; then
+		REAIM_EXCLUDE=none
+	fi
 	$SHELLPACK_INCLUDE/shellpack-bench-reaim \
 		--filesize      $REAIM_FILESIZE \
 		--poolsize      $REAIM_POOLSIZE \
 		--startusers    $REAIM_STARTUSERS \
 		--endusers      $REAIM_ENDUSERS \
 		--increment     $REAIM_INCREMENT \
-		--jobs-per-user $REAIM_JOBS_PER_USER \
 		--workfile      $REAIM_WORKFILE \
+		--exclude       $REAIM_EXCLUDE \
 		--iterations    $REAIM_ITERATIONS
 	return $?
 }
