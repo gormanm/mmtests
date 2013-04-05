00ce51facbc11c75b96e20291e4b8349ffaf49a7
Mel Gorman <mgorman@suse.de>
2013-04-05 10:27:15 +0100

    monitor-top: Add option to only record active processes in top
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- monitors/watch-top.sh
+++ monitors/watch-top.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-exec top -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
-	if ($_ =~ /^top -.*/) {
-		print "time: " . time . "\n";
-	}
-	print $_;}'
+if [ "$MONITOR_TOP_ACTIVEONLY" != "yes" ]; then
+	exec top -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
+		if ($_ =~ /^top -.*/) {
+			print "time: " . time . " full\n";
+		}
+		print $_;
+	}'
+else
+	exec top -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
+		if ($_ =~ /^top -.*/) {
+			print "time: " . time . " short\n";
+		}
+		my $line = $_;
+		my $filter = $line;
+		$filter =~ s/^\s+//;
+
+		my @elements = split(/\s+/, $filter);
+		if ($elements[0] !~ /[0-9]+/ || $elements[8] ne "0.0") {
+			print $line;
+		}
+	}'
+fi
