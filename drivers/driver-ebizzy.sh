c88d94680631f44c2af90ce1cf4440e370c64d14
Mel Gorman <mgorman@suse.de>
2013-05-22 09:11:37 +0100

    ebizzy: Add ebizzy benchmark
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ drivers/driver-ebizzy.sh
@@ -0,0 +1,10 @@
+FINEGRAINED_SUPPORTED=yes
+NAMEEXTRA=
+
+run_bench() {
+	$SHELLPACK_INCLUDE/shellpack-bench-ebizzy \
+		--max-threads $EBIZZY_MAX_THREADS \
+		--iterations $EBIZZY_ITERATIONS \
+		--duration $EBIZZY_DURATION
+	return $?
+}
