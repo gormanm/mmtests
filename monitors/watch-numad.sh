678df41eedea8ff8b7e38c7c8a17be96d9696496
Mel Gorman <mgorman@suse.de>
2013-05-29 15:44:24 +0100

    watch-numad: Add monitor to start numad daemon and log output
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ monitors/watch-numad.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+install-depends numad
+
+# We're managing numad now
+/etc/init.d/numad stop || exit -1
+NUMAD_BIN=/usr/sbin/numad
+if [ ! -e $NUMAD_BIN ]; then
+	NUMAD_BIN=/usr/bin/numad
+fi
+if [ ! -e $NUMAD_BIN ]; then
+	exit -1
+fi
+
+TAILPID=
+
+shutdown_numad() {
+	$NUMAD_BIN -i 0
+	kill $TAILPID
+	exit 0
+}
+	
+trap shutdown_read SIGTERM
+
+$NUMAD_BIN -i 5 || exit -1
+tail -1f /var/log/numad.log &
+TAILPID=$!
+wait $TAILPID
