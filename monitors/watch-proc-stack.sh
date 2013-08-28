628bf22f0f386aea8aab3023fbbee7ce735b8b1b
Mel Gorman <mgorman@suse.de>
2013-08-28 22:39:01 +0100

    proc-stack: Monitor all threads as well
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- monitors/watch-proc-stack.sh
+++ monitors/watch-proc-stack.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+cd /proc
 while [ 1 ]; do
 	echo time: `date +%s`
 
-	for PROC in `\ls -d /proc/[0-9]*`; do
-		if [ -e $PROC/status ]; then
-			cat $PROC/status
-			cat $PROC/stack
+	for PROC in [0-9]*; do
+		LIST=
+		if [ -e /proc/$PROC/status ]; then
+			LIST="/proc/$PROC/status /proc/$PROC/stat /proc/$PROC/stack"
 		fi
+
+		# Now process all its threads
+		for THREAD_PROC in /proc/$PROC/task/[0-9]*; do
+			if [ "$THREAD_PROC" = "/proc/$PROC/task/$PROC" ]; then
+				continue
+			fi
+			LIST="$LIST $THREAD_PROC/status $THREAD_PROC/stat $THREAD_PROC/stack"
+		done
+		cat $LIST 2> /dev/null
 	done
 	sleep $MONITOR_UPDATE_FREQUENCY
 done
