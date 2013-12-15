3436a54836ad6e81229362d89eebb22664f84308
Mel Gorman <mgorman@suse.de>
2013-12-15 11:09:05 +0000

    tlbflush: Hack around threading error in benchmark
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- bin/lib/MMTests/ExtractTlbflush.pm
+++ bin/lib/MMTests/ExtractTlbflush.pm
@@ -41,7 +41,7 @@ sub initialise() {
 	$self->{_FieldFormat} = [ "%-${fieldLength}d", "%${fieldLength}d", "%$fieldLength.4f",
 				  "%$fieldLength.4f",  "%$fieldLength.4f", "%$fieldLength.4f" ];
 	$self->{_FieldHeaders} = [ "Client", "Iteration", "Records/sec", "User", "Sys" ];
-	$self->{_SummaryHeaders} = [ "Client", "Min", "Mean", "TrueMean", "Stddev", "Max" ];
+	$self->{_SummaryHeaders} = [ "Client", "Min", "Mean", "Range", "Stddev", "Max" ];
 }
 
 sub printPlot() {
@@ -101,7 +101,7 @@ sub extractSummary() {
 			push @units, @{$row}[$column];
 		}
 		push @row, $client;
-		foreach my $funcName ("calc_min", "calc_mean", "calc_true_mean", "calc_stddev", "calc_max") {
+		foreach my $funcName ("calc_min", "calc_mean", "calc_range", "calc_stddev", "calc_max") {
 			no strict "refs";
 			push @row, &$funcName(@units);
 		}
