e9b7a2b9848dbc8ed7ab849363e8b181acfb1daf
Mel Gorman <mgorman@suse.de>
2013-12-16 10:26:30 +0000

    ebizzy: Report per-thread performance
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ bin/lib/MMTests/CompareEbizzyrange.pm
@@ -0,0 +1,19 @@
+# CompareEbizzyrange.pm
+package MMTests::CompareEbizzyrange;
+use MMTests::Compare;
+our @ISA = qw(MMTests::Compare); 
+
+sub new() {
+	my $class = shift;
+	my $self = {
+		_ModuleName  => "CompareEbizzyrange",
+		_DataType    => MMTests::Compare::DATA_THROUGHPUT,
+		_FieldLength => 12,
+		_CompareOps  => [ "none", "pndiff", "pndiff", "pndiff", "pdiff", "pndiff", "pndiff" ],
+		_ResultData  => []
+	};
+	bless $self, $class;
+	return $self;
+}
+
+1;
