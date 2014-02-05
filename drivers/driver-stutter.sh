e9fd5ffab485d6293a856ed64f91ef8123431159
Mel Gorman <mgorman@suse.de>
2014-02-05 16:41:27 +0000

    stutter: Add test that hogs memory using tmpfs
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- drivers/driver-stutter.sh
+++ drivers/driver-stutter.sh
@@ -2,9 +2,10 @@ FINEGRAINED_SUPPORTED=no
 NAMEEXTRA=
 
 run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-stutter	\
-		--memfault-size $STUTTER_MEMFAULT_SIZE	\
-		--filesize $STUTTER_FILESIZE		\
+	$SCRIPTDIR/shellpacks/shellpack-bench-stutter		\
+		--memfault-size $STUTTER_MEMFAULT_SIZE		\
+		--memfault-tmpfs $STUTTER_MEMFAULT_TMPFS	\
+		--filesize $STUTTER_FILESIZE			\
 		--blocksize $STUTTER_BLOCKSIZE
 	return $?
 }
