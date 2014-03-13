7a6acf44f56213027fa99379e5f974ee31ffd923
Mel Gorman <mgorman@suse.de>
2014-03-13 11:00:47 +0000

    bonnie: Add reporting scripts
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ bin/lib/MMTests/CompareBonnie.pm
@@ -0,0 +1,18 @@
+# CompareBonnie.pm
+package MMTests::CompareBonnie;
+use MMTests::Compare;
+our @ISA = qw(MMTests::Compare); 
+
+sub new() {
+	my $class = shift;
+	my $self = {
+		_ModuleName  => "CompareBonnie",
+		_DataType    => MMTests::Compare::DATA_OPSSEC,
+		_FieldLength => 16,
+		_ResultData  => []
+	};
+	bless $self, $class;
+	return $self;
+}
+
+1;
