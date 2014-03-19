2f90cce380ae84e466948755bc783019f7617722
Mel Gorman <mgorman@suse.de>
2014-03-19 10:32:58 +0000

    trinity: Update trinity benchmark to optionall run VM group of tests
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- drivers/driver-trinity.sh
+++ drivers/driver-trinity.sh
@@ -2,6 +2,17 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
-	$SHELLPACK_INCLUDE/shellpack-bench-trinity
+	GROUP_PARAM=
+	if [ "$TRINITY_GROUP" != "" ]; then
+		GROUP_PARAM="--group $TRINITY_GROUP"
+	fi
+
+	SYSCALL_PARAM=
+	if [ "$TRINITY_SYSCALL" != "" ]; then
+		SYSCALL_PARAM="--syscall $TRINITY_SYSCALL"
+	fi
+
+	$SHELLPACK_INCLUDE/shellpack-bench-trinity $SYSCALL_PARAM $GROUP_PARAM \
+		--duration $TRINITY_DURATION
 	return $?
 }
