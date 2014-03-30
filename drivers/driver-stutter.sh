e325989435dab7497b7fd68c18e57e88cca1a772
Mel Gorman <mgorman@suse.de>
2014-03-30 21:19:24 +0100

    stutter: Detect if local disk is too small
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- drivers/driver-stutter.sh
+++ drivers/driver-stutter.sh
@@ -2,7 +2,11 @@ FINEGRAINED_SUPPORTED=no
 NAMEEXTRA=
 
 run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-stutter		\
+	LOCAL_PARAM=
+	if [ "$STUTTER_USE_LOCAL" = "yes" ]; then
+		LOCAL_PARAM=--source-local
+	fi
+	$SCRIPTDIR/shellpacks/shellpack-bench-stutter $LOCAL_PARAM \
 		--memfault-size $STUTTER_MEMFAULT_SIZE		\
 		--memfault-tmpfs $STUTTER_MEMFAULT_TMPFS	\
 		--filesize $STUTTER_FILESIZE			\
