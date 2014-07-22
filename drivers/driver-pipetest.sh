03b23f7cf7393102a647feea83c6ceb1aed7a51f
Mel Gorman <mgorman@suse.de>
2014-07-22 16:04:40 +0100

    pipetest: Allow bindings to be specified
    Although only a pinned makes any sense.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- drivers/driver-pipetest.sh
+++ drivers/driver-pipetest.sh
@@ -2,7 +2,11 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
+	BIND_SWITCH=
+	if [ "$PIPETEST_BINDING" != "" ]; then
+		BIND_SWITCH=--bind-$PIPETEST_BINDING
+	fi
 	$SHELLPACK_INCLUDE/shellpack-bench-pipetest \
-		-i $PIPETEST_ITERATIONS
+		-i $PIPETEST_ITERATIONS $BIND_SWITCH
 	return $?
 }
