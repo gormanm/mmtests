7a0d0598c00ebd37e795355079b13bf29b06e1d7
Mel Gorman <mgorman@suse.de>
2014-07-25 10:59:21 +0100

    netperf: Add reporting scripts for rr
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ bin/lib/MMTests/CompareNetperftcprr.pm
@@ -0,0 +1,6 @@
+# CompareNetperftcprr.pm
+package MMTests::CompareNetperftcprr;
+use MMTests::CompareNetperf;
+our @ISA = qw(MMTests::CompareNetperf);
+
+1;
---
+++ bin/lib/MMTests/ExtractNetperfudprr.pm
@@ -0,0 +1,6 @@
+# ExtractNetperfudprr.pm
+package MMTests::ExtractNetperfudprr;
+use MMTests::ExtractNetperf;
+our @ISA = qw(MMTests::ExtractNetperf);
+
+1;
