76ef9f8a241b2a32fe73d8609fde22a2cc2850fc
Mel Gorman <mgorman@suse.de>
2014-10-03 15:27:07 +0100

    pgioperf: Add reporting scripts
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ bin/lib/MMTests/ComparePgioperf.pm
@@ -0,0 +1,18 @@
+# ComparePgioperf.pm
+package MMTests::ComparePgioperf;
+use MMTests::Compare;
+our @ISA = qw(MMTests::Compare); 
+
+sub new() {
+	my $class = shift;
+	my $self = {
+		_ModuleName  => "ComparePgioperf",
+		_DataType    => MMTests::Compare::DATA_OPSSEC,
+		_FieldLength => 16,
+		_ResultData  => []
+	};
+	bless $self, $class;
+	return $self;
+}
+
+1;
