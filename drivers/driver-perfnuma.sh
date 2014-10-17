aee6a6065e3f3b90e123dd4ec7716f876d7fe741
Mel Gorman <mgorman@suse.de>
2014-10-17 17:10:31 +0100

    perfnuma: Add basic convergence test
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ drivers/driver-perfnuma.sh
@@ -0,0 +1,17 @@
+FINEGRAINED_SUPPORTED=yes
+NAMEEXTRA=
+
+run_bench() {
+	VERSION_PARAM=
+	if [ "$PERFNUMA_VERSION" != "" ]; then
+		VERSION_PARAM="-v $PERFNUMA_VERSION"
+	fi
+
+	$SHELLPACK_INCLUDE/shellpack-bench-perfnuma $VERSION_PARAM	\
+		--nr-processes $PERFNUMA_NR_PROCESSES			\
+		--nr-threads   $PERFNUMA_NR_THREADS			\
+		--process-wss  $PERFNUMA_PROCESS_WSS			\
+		--workloads    "$PERFNUMA_WORKLOADS"			\
+		--iterations   $PERFNUMA_ITERATIONS
+	return $?
+}
