df80d58b018b025127e86a24845c59d6e6aceeda
Mel Gorman <mgorman@suse.de>
2015-03-30 15:11:51 +0100

    netperf: Report send, receive and loss for UDP_STREAM
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- bin/lib/MMTests/ExtractNetperf.pm
+++ bin/lib/MMTests/ExtractNetperf.pm
@@ -37,13 +37,15 @@ sub extractReport($$$) {
 	}
 	@sizes = sort {$a <=> $b} @sizes;
 
+	my $loss;
 	foreach my $size (@sizes) {
 		my $file = "$reportDir/noprofile/$protocol-$size.log";
 		my $confidenceLimit;
-		my $throughput;
 		my $iteration = 0;
 
 		foreach $file (<$reportDir/noprofile/$protocol-$size.*>) {
+			my $send_tput = 0;
+			my $recv_tput = 0;
 			open(INPUT, $file) || die("Failed to open $file\n");
 			while (<INPUT>) {
 				my @elements = split(/\s+/, $_);
@@ -56,15 +58,47 @@ sub extractReport($$$) {
 					next;
 				}
 				my @elements = split(/\s+/, $_);
-				if ($#elements > 3) {
-					$throughput = $elements[-1];
+				if ($protocol ne "UDP_STREAM") {
+					if ($#elements > 3) {
+						$send_tput = $elements[-1];
+					}
+				} else {
+					if ($#elements == 6) {
+						$send_tput = $elements[-1];
+					}
+					if ($#elements == 4) {
+						$recv_tput = $elements[-1];
+					}
 				}
 			}
 			close(INPUT);
-			push @{$self->{_ResultData}}, [ $size, ++$iteration, $throughput ];
+			if ($protocol ne "UDP_STREAM") {
+				push @{$self->{_ResultData}}, [ $size, ++$iteration, $send_tput ];
+			} else {
+				push @{$self->{_ResultData}}, [ "send-$size", ++$iteration, $send_tput ];
+				push @{$self->{_ResultData}}, [ "recv-$size", ++$iteration, $recv_tput ];
+				if (($send_tput - $recv_tput) / 10 > $send_tput) {
+					push @{$self->{_ResultData}}, [ "loss-$size", ++$iteration, $send_tput - $recv_tput ];
+					$loss++;
+				}
+			}
 		}
 	}
-	$self->{_Operations} = \@sizes;
+	if ($protocol ne "UDP_STREAM") {
+		$self->{_Operations} = \@sizes;
+	} else {
+		my @ops;
+		my @directions = ("send", "recv");
+		if ($loss) {
+			push @directions, "loss";
+		}
+		foreach my $direction (@directions) {
+			foreach my $size (@sizes) {
+				push @ops, "$direction-$size";
+			}
+		}
+		$self->{_Operations} = \@ops;
+	}
 }
 
 1;
