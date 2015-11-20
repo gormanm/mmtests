64da80b0c85afa65059925173c2dd8ed3a640115
Mel Gorman <mgorman@techsingularity.net>
2015-11-20 11:01:52 +0000

    johnripper: Include reporting scripts for user/system CPU usage
    Signed-off-by: Mel Gorman <mgorman@suse.de>

---
+++ bin/lib/MMTests/CompareJohnripperexectime.pm
@@ -0,0 +1,19 @@
+# CompareJohnripperexectime.pm
+package MMTests::CompareJohnripperexectime;
+use MMTests::Compare;
+our @ISA = qw(MMTests::Compare);
+
+sub new() {
+	my $class = shift;
+	my $self = {
+		_ModuleName  => "CompareJohnripperexectime",
+		_DataType    => MMTests::Compare::DATA_TIME_SECONDS,
+		_CompareOps  => [ "pndiff", "pndiff", "pndiff", "pndiff", "pndiff", "pndiff" ],
+		_FieldLength => 12,
+		_ResultData  => []
+	};
+	bless $self, $class;
+	return $self;
+}
+
+1;
