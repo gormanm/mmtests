d97a00b7a895a327a2b8aa1df47fca444c0cf11a
Mel Gorman <mgorman@techsingularity.net>
2016-01-07 13:58:35 +0000

    sqlite: Include warmup data if none other is available
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/lib/MMTests/ExtractSqlite.pm
+++ bin/lib/MMTests/ExtractSqlite.pm
@@ -19,17 +19,27 @@ sub new() {
 
 sub extractReport($$$) {
 	my ($self, $reportDir, $reportName) = @_;
-
+	my $exclude_warmup = 0;
 	my $file = "$reportDir/noprofile/sqlite.log";
+
 	open(INPUT, $file) || die("Failed to open $file\n");
-	my $nr_sample = 0;
 	while (<INPUT>) {
 		my @elements = split(/\s+/);
 		next if $elements[0] eq "warmup";
+
+		$exclude_warmup = 1;
+		last;
+	}
+	seek(INPUT, 0, 0);
+
+	my $nr_sample = 0;
+	while (<INPUT>) {
+		my @elements = split(/\s+/);
+		next if $exclude_warmup && $elements[0] eq "warmup";
 		push @{$self->{_ResultData}}, ["Trans", $nr_sample++, $elements[1]];
 	}
+	close INPUT;
 
 	$self->{_Operations} = [ "Trans" ];
-	close INPUT;
 }
 1;
