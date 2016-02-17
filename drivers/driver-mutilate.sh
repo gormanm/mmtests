64ec0851cf04550a4de541024b746c6de3bb467a
Mel Gorman <mgorman@techsingularity.net>
2016-02-17 10:00:52 +0000

    mutilate: Allow minimum number of threads to be specified
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-mutilate.sh
+++ drivers/driver-mutilate.sh
@@ -3,6 +3,7 @@ NAMEEXTRA=
 
 run_bench() {
 	$SHELLPACK_INCLUDE/shellpack-bench-mutilate \
+		--min-threads $MUTILATE_MIN_THREADS \
 		--max-threads $MUTILATE_MAX_THREADS \
 		--iterations $MUTILATE_ITERATIONS \
 		--duration $MUTILATE_DURATION \
