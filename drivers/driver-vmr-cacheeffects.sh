5e38cc170da7b197c44f6d9c5f37b07062e02eec
Mel Gorman <mgorman@techsingularity.net>
2016-03-04 11:54:37 +0000

    vmr-stream: remove
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-vmr-cacheeffects.sh
+++
@@ -1,7 +0,0 @@
-FINEGRAINED_SUPPORTED=no
-NAMEEXTRA=
-
-run_bench() {
-	taskset -c 0 $SCRIPTDIR/shellpacks/shellpack-bench-vmr-cacheeffects
-	return $?
-}
--- drivers/driver-vmr-createdelete.sh
+++
@@ -1,7 +0,0 @@
-FINEGRAINED_SUPPORTED=no
-NAMEEXTRA=
-
-run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-vmr-createdelete
-	return $?
-}
