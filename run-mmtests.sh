36805c47ea347fc0354201a5017680942435e785
Mel Gorman <mgorman@techsingularity.net>
2016-04-28 14:15:33 +0100

    run-mmtests: Adapt to changes in mdadm output
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- run-mmtests.sh
+++ run-mmtests.sh
@@ -370,6 +370,7 @@ if [ "$TESTDISK_RAID_DEVICES" != "" ]; then
 spawn mdadm --create $TESTDISK_RAID_MD_DEVICE -l $TESTDISK_RAID_TYPE -n $NR_DEVICES $TESTDISK_RAID_PARTITIONS
 expect {
 	"Continue creating array?" { send yes\\r; exp_continue}
+	"Wipe it"		   { send y\\e; exp_continue}
 }
 EOF
 			expect -f $EXPECT_SCRIPT || exit -1
@@ -382,6 +383,7 @@ EOF
 spawn mdadm --create $TESTDISK_RAID_MD_DEVICE --bitmap=internal -l $TESTDISK_RAID_TYPE -n $NR_DEVICES $TESTDISK_RAID_PARTITIONS
 expect {
 	"Continue creating array?" { send yes\\r; exp_continue}
+	"Wipe it"		   { send y\\e; exp_continue}
 }
 EOF
 			expect -f $EXPECT_SCRIPT || exit -1
@@ -395,6 +397,7 @@ EOF
 spawn mdadm --create $TESTDISK_RAID_MD_DEVICE -l $TESTDISK_RAID_TYPE -n $NR_DEVICES $TESTDISK_RAID_PARTITIONS
 expect {
 	"Continue creating array?" { send yes\\r; exp_continue}
+	"Wipe it"		   { send y\\e; exp_continue}
 }
 EOF
 			expect -f $EXPECT_SCRIPT || exit -1
