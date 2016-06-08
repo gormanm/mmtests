a12cbc8f4f1ce170eb8e38d5bb2105d94722051e
Mel Gorman <mgorman@techsingularity.net>
2016-06-08 09:29:09 +0100

    unixbench-syscall: Correct comparison
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/lib/MMTests/CompareUnixbenchsyscall.pm
+++ bin/lib/MMTests/CompareUnixbenchsyscall.pm
@@ -1,6 +1,19 @@
 # CompareUnixbenchsyscall.pm
 package MMTests::CompareUnixbenchsyscall;
-use MMTests::CompareUnixbenchcommon;
-our @ISA = qw(MMTests::CompareUnixbenchcommon);
+use MMTests::Compare;
+our @ISA = qw(MMTests::Compare);
+
+sub new() {
+	my $class = shift;
+	my $self = {
+		_ModuleName  => "CompareUnixbenchsyscall",
+		_DataType    => MMTests::Compare::DATA_TIME_SECONDS,
+		_FieldLength => 12,
+		_CompareOp   => "pndiff",
+		_ResultData  => []
+	};
+	bless $self, $class;
+	return $self;
+}
 
 1;
