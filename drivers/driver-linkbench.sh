c4b771ed86c9d235b2a8f2b691466e318feb86d9
Mel Gorman <mgorman@techsingularity.net>
2016-08-22 23:39:53 +0100

    linkbench: Do not pass workload size as parameter
    Only the install script requires it and it's picked up indirectly by
    the environment.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-linkbench.sh
+++ drivers/driver-linkbench.sh
@@ -6,7 +6,6 @@ run_bench() {
 			--warmup-time	$LINKBENCH_WARMUP_TIME \
 			--min-threads	$LINKBENCH_MIN_THREADS \
 			--max-threads	$LINKBENCH_MAX_THREADS \
-			--workload-size $LINKBENCH_WORKLOAD_SIZE \
 			--iterations    $LINKBENCH_ITERATIONS
 
 	return $?
