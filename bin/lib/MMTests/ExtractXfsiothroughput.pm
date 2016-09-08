c63e1a28659a23bdba6c0ba5346717213dd292ce
Mel Gorman <mgorman@techsingularity.net>
2016-09-08 13:17:45 +0100

    xfsio: Handle different speeds
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/lib/MMTests/ExtractXfsiothroughput.pm
+++ bin/lib/MMTests/ExtractXfsiothroughput.pm
@@ -32,8 +32,19 @@ sub extractReport($$$) {
 
 		open(INPUT, $file) || die("Failed to open $file\n");
 		while (<INPUT>) {
-			next if $_ !~ /.*\(([0-9.]+) MiB\/sec.*/;
-			push @{$self->{_ResultData}}, [ "$testcase-tput", ++$iteration, $1 ];
+			my $tput;
+
+			if ($_ =~ /.*\(([0-9.]+) MiB\/sec.*/) {
+				$tput = $1;
+			} elsif ($_ =~ /.*\(([0-9.]+) GiB\/sec.*/) {
+				$tput = $1 * 1024;
+			} elsif ($_ =~ /.*\(([0-9.]+) KiB\/sec.*/) {
+				$tput = $1 / 1024;
+			} else {
+				next;
+			}
+
+			push @{$self->{_ResultData}}, [ "$testcase-tput", ++$iteration, $tput ];
 		}
 		close(INPUT);
 	}
