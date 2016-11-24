434e39089354d5e4be0ef3e710429b6efcceaa2f
root <root@delboy.163woodhaven.lan>
2016-11-24 11:45:48 +0000

    pagealloc: Do not use probe context for executing test
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/stap-fix.sh
+++ bin/stap-fix.sh
@@ -49,7 +49,7 @@ fi
 
 echo WARNING: systemtap installation broken, trying to fix.
 
-for PATCH in 4.5 4.6 4.6-rc3 4.7-rc1; do
+for PATCH in 4.5 4.6 4.6-rc3 4.7-rc1 4.8; do
 	cat $SCRIPTDIR/stap-patches/systemtap-runtime-${PATCH}.patch | patch -p1 -d /usr/share/systemtap
 	if [ $? -ne 0 ]; then
 		restore_systemtap
