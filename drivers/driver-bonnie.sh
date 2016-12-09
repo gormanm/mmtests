3c5a7ae028efce1b289f2e3e3549faa78c34f359
Jan Kara <jack@suse.cz>
2016-12-09 19:46:47 +0000

    bonnie: Split bonnie runs to file and directory parts
    File parts are noticeably slower than directory parts of bonnie runs.
    Furthermore they tend to be more stable to split them out and keep
    performing them three times. Perform directory part of bonnie runs 5
    times for fsync mode and 10 times for async runs.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-bonnie.sh
+++ drivers/driver-bonnie.sh
@@ -5,9 +5,15 @@ run_bench() {
 	if [ "$BONNIE_FSYNC" = "yes" ]; then
 		BONNIE_FSYNC_PARAM="--sync"
 	fi
+	if [ "$BONNIE_NR_FILES" != "" ]; then
+		FILES_PARAM="--nr_files $BONNIE_NR_FILES --dirsize $BONNIE_DIRECTORY_SIZE"
+	fi
+	if [ "$BONNIE_DATASET_SIZE" != "" ]; then
+		SIZE_PARAM="--dataset $BONNIE_DATASET_SIZE"
+	fi
 	$SHELLPACK_INCLUDE/shellpack-bench-bonnie $BONNIE_FSYNC_PARAM \
-		--dataset  $BONNIE_DATASET_SIZE \
-		--nr_files $BONNIE_NR_FILES \
+		$SIZE_PARAM \
+		$FILES_PARAM \
 		--nr_directories $BONNIE_NR_DIRECTORIES \
 		--iterations $BONNIE_ITERATIONS
 	return $?
