a92d54df9a77d25c9a11a93d6b6d39bc2e9bffa4
Matt Fleming <matt@codeblueprint.co.uk>
2017-03-15 09:17:59 +0000

    watch-perf-sched: Enable schedstats for more sched tracepoints
    In particular, this is required for the sched:sched_stat_wait
    tracepoint to be enabled which records runqueue latency (time between
    task wakeup and that task getting the CPU).
    
    'sched_schedstats' only became a tunable in v4.6, before that it was
    always enabled.
    
    Signed-off-by: Matt Fleming <matt@codeblueprint.co.uk>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- monitors/watch-perf-sched.sh
+++ monitors/watch-perf-sched.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -e /proc/sys/kernel/sched_schedstats ]; then
+	echo 1 > /proc/sys/kernel/sched_schedstats
+fi
+
 # Run perf sched system-wide 
 exec perf sched record -o $MONITOR_LOG \
 	perl -e "open(OUTPUT, \">$MONITOR_PID\"); print OUTPUT \$\$; close OUTPUT; sleep;"
