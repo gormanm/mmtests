b19d0f5dfca6324563f225e7db258c3c3cbf49b6
Mel Gorman <mgorman@techsingularity.net>
2017-04-13 19:38:00 +0100

    pgbench: Include more results
    Signed-off-by: Mel Gorman <mgorman@suse.com>

--- bin/lib/MMTests/ExtractPgbench.pm
+++ bin/lib/MMTests/ExtractPgbench.pm
@@ -32,12 +32,10 @@ sub extractReport() {
 		push @clients, $split[-1];
 	}
 	@clients = sort { $a <=> $b } @clients;
-	my $stallStart = 0;
 
 	# Extract per-client transaction information
 	foreach my $client (@clients) {
 		my $sample = 0;
-		my $stallStart = 0;
 		my @values;
 		my $startSamples = 1;
 		my $endSamples = 0;
@@ -52,35 +50,36 @@ sub extractReport() {
 		close(INPUT);
 
 		# Find where the shutdown cutoff is where transactions start to taper
-		if ($endSamples > 60) {
-			# Find where we should stop recording samples
-			my $maxIndex = $endSamples - 30;
-			my $maxValue = $values[$maxIndex];
-			for (my $i = $endSamples - 30; $i < $endSamples; $i++) {
-				if ($values[$i] >= $maxValue) {
-					$maxIndex = $i;
-					$maxValue = $values[$i];
-				}
-			}
-			$endSamples = $maxIndex;
-
-			# Find where we should start recording samples
-			$maxIndex = 0;
-			$maxValue = $values[$maxIndex];
-			for (my $i = 0; $i < 30; $i++) {
-				if ($values[$i] >= $maxValue) {
-					$maxIndex = $i;
-					$maxValue = $values[$i];
-				}
-			}
-			$startSamples = $maxIndex;
-		}
+#		if ($endSamples > 60) {
+#			# Find where we should stop recording samples
+#			my $maxIndex = $endSamples - 30;
+#			my $maxValue = $values[$maxIndex];
+#			for (my $i = $endSamples - 30; $i < $endSamples; $i++) {
+#				if ($values[$i] >= $maxValue) {
+#					$maxIndex = $i;
+#					$maxValue = $values[$i];
+#				}
+#			}
+#			$endSamples = $maxIndex;
+#
+#			# Find where we should start recording samples
+#			$maxIndex = 0;
+#			$maxValue = $values[$maxIndex];
+#			for (my $i = 0; $i < 30; $i++) {
+#				if ($values[$i] >= $maxValue) {
+#					$maxIndex = $i;
+#					$maxValue = $values[$i];
+#				}
+#			}
+#			$startSamples = $maxIndex;
+#		}
 
 		my $testStart = 0;
 		my $sumTransactions = 0;
 		my $nr_readings = 0;
 		my $thisBatch = 0;
-		my $batch = int (($endSamples - $startSamples) / 12);
+		# my $batch = int (($endSamples - $startSamples) / 12);
+		my $batch = 4;
 		if ($batch <= 0) {
 			$batch = 1;
 		}
@@ -98,7 +97,11 @@ sub extractReport() {
 				$thisBatch++;
 				$sumTransactions += $nrTransactions;
 				if ($thisBatch % $batch == 0) {
-					push @{$self->{_ResultData}}, [ $client, $elements[0] - $testStart, $sumTransactions / $batch ];
+					my $trans = $sumTransactions / $batch;
+					if ($trans == 0) {
+						$trans = 1;
+					}
+					push @{$self->{_ResultData}}, [ $client, $elements[0] - $testStart, $trans ];
 					$thisBatch = 0;
 					$sumTransactions = 0;
 					$nr_readings++;
