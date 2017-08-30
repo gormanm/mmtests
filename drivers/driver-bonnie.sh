3248941d5663bef772d675523ff322f117321707
Mel Gorman <mgorman@techsingularity.net>
2017-08-30 21:24:41 +0100

    bonnie: Handle directories not being specified correctly
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-bonnie.sh
+++ drivers/driver-bonnie.sh
@@ -8,13 +8,17 @@ run_bench() {
 	if [ "$BONNIE_NR_FILES" != "" ]; then
 		FILES_PARAM="--nr_files $BONNIE_NR_FILES --dirsize $BONNIE_DIRECTORY_SIZE"
 	fi
+	if [ "$BONNIE_NR_DIRECTORIES" != "" ]; then
+		DIRS_PARAM="--nr_directories $BONNIE_NR_DIRECTORIES"
+	fi
 	if [ "$BONNIE_DATASET_SIZE" != "" ]; then
 		SIZE_PARAM="--dataset $BONNIE_DATASET_SIZE"
 	fi
+
 	$SHELLPACK_INCLUDE/shellpack-bench-bonnie $BONNIE_FSYNC_PARAM \
 		$SIZE_PARAM \
 		$FILES_PARAM \
-		--nr_directories $BONNIE_NR_DIRECTORIES \
+		$DIRS_PARAM \
 		--iterations $BONNIE_ITERATIONS
 	return $?
 }
