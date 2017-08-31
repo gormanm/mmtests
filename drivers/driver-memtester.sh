6b961f4f86d9ecf01f40d83efc43c574b3d5ba7c
Mel Gorman <mgorman@techsingularity.net>
2017-08-31 14:35:12 +0100

    memtester: Remove obsolete installer
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-memtester.sh
+++
@@ -1,9 +0,0 @@
-FINEGRAINED_SUPPORTED=no
-NAMEEXTRA=
-
-run_bench() {
-	$SHELLPACK_INCLUDE/shellpack-bench-memtester \
-		--instances $MEMTESTER_INSTANCES \
-		--mb-usage  $MEMTESTER_MEMORY_MB
-	return $?
-}
