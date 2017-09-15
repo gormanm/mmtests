90af135214359dc91b5c7299324d43f1e221be4a
Mel Gorman <mgorman@techsingularity.net>
2017-09-15 17:46:59 +0100

    thpscale: Insert dummy values if necessary
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/lib/MMTests/ExtractThpscale.pm
+++ bin/lib/MMTests/ExtractThpscale.pm
@@ -29,9 +29,11 @@ sub extractReport() {
 	}
 	@clients = sort { $a <=> $b } @clients;
 
+
 	foreach my $client (@clients) {
 		my $faults = 0;
 		my $inits = 0;
+		my %seen;
 
 		my $file = "$reportDir/$profile/threads-$client.log";
 		open(INPUT, $file) || die("Failed to open $file\n");
@@ -40,9 +42,17 @@ sub extractReport() {
 			if ($line =~ /^fault/) {
 				my @elements = split(/\s+/, $line);
 				push @{$self->{_ResultData}}, [ "fault-$elements[2]-$client", ++$faults, $elements[3] ];
+				$seen{$elements[2]} = 1;
 			}
 		}
 		close INPUT;
+
+		if ($seen{"base"} != 1) {
+			push @{$self->{_ResultData}}, [ "fault-base-$client", 1, 0 ];
+		}
+		if ($seen{"huge"} != 1) {
+			push @{$self->{_ResultData}}, [ "fault-huge-$client", 1, 0 ];
+		}
 	}
 
 	foreach my $client (@clients) {
