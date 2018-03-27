f8ef504fc254f4c2d6dd2751274f9a9913d4d732
Mel Gorman <mgorman@techsingularity.net>
2018-03-27 12:21:39 +0100

    nas: Specify a default parameter
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-nas-mpi.sh
+++ drivers/driver-nas-mpi.sh
@@ -6,6 +6,9 @@ run_bench() {
 	if [ "$NAS_USE_CMA" = "yes" ]; then 
 		CMAOPT="--cma"
 	fi
+	if [ "$NAS_MAX_CPUS" = "" ]; then
+		NAS_MAX_CPUS=$NUMCPUS
+	fi
 	$SCRIPTDIR/shellpacks/shellpack-bench-nas $CMAOPT	\
 		--type MPI $CMAOPT				\
 		--max-cpus $NAS_MAX_CPUS
--- drivers/driver-nas-omp.sh
+++ drivers/driver-nas-omp.sh
@@ -2,6 +2,9 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
+	if [ "$NAS_MAX_CPUS" = "" ]; then
+		NAS_MAX_CPUS=$NUMCPUS
+	fi
 	$SCRIPTDIR/shellpacks/shellpack-bench-nas	\
 		--type OMP				\
 		--max-cpus $NAS_MAX_CPUS
