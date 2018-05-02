995e34ffcd74cb7f132853524b9d4a363ba2965e
Mel Gorman <mgorman@techsingularity.net>
2018-05-02 16:04:02 +0100

    usemem: Allow test case using memory as a fake swap device and compaction
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-usemem.sh
+++ drivers/driver-usemem.sh
@@ -2,7 +2,12 @@ FINEGRAINED_SUPPORTED=yes
 NAMEEXTRA=
 
 run_bench() {
-	eval $SHELLPACK_INCLUDE/shellpack-bench-usemem \
+	USEMEM_FAKE_SWAP_MB_SWITCH=
+	if [ "$USEMEM_FAKE_SWAP_MB" != "" ]; then
+		USEMEM_FAKE_SWAP_MB_SWITCH="--fake-swap \"$USEMEM_FAKE_SWAP_MB\""
+	fi
+
+	eval $SHELLPACK_INCLUDE/shellpack-bench-usemem $USEMEM_FAKE_SWAP_MB_SWITCH \
 		--size        		$USEMEM_WORKLOAD_SIZE	\
 		--anon-percentage	$USEMEM_PERCENTAGE_ANON \
 		--loops			$USEMEM_LOOPS		\
