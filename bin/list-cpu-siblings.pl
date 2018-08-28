2cddddc94c92d68286f273f0d2609715f97cc946
Andreas Herrmann <aherrmann@suse.com>
2018-08-28 14:59:20 +0100

    mmtests: list-cpu-topology on mulit-node AMD
    mmtests way of listing CPU topology does not work for AMD multi-node
    CPUs.
    
    You can spot the difference between "Memory Topology" and "CPU
    Topology" at
    http://laplace.suse.de/marvin/SLE-12-SP4-SLE-15/machines/desc-molitor
    
    The reason is that list-cpu-topology.sh/list-cpu-siblings.pl rely on
    information from
    /sys/bus/cpu/drivers/processor/cpu*/topology/core_siblings and assume
    that a physical package does not contain CPUs of more than one node.
    In reality topology/core_siblings lists all CPUs with the same
    physical package ID and in case of multi-node CPUs there are two nodes
    in one package.
    
    Thus we generate incorrect information, e.g.
    
    molitor:~/:[0]# git-private/mmtests-0.16/bin/list-cpu-toplogy.sh
    node 0 socket 0 core 0 thread 0 cpu 0
    node 0 socket 0 core 0 thread 1 cpu 2
    node 0 socket 0 core 2 thread 0 cpu 4
    node 0 socket 0 core 2 thread 1 cpu 6
    node 0 socket 0 core 4 thread 0 cpu 8
    node 0 socket 0 core 4 thread 1 cpu 10
    node 0 socket 0 core 6 thread 0 cpu 12
    node 0 socket 0 core 6 thread 1 cpu 14
    node 0 socket 0 core 8 thread 0 cpu 16
    node 0 socket 0 core 8 thread 1 cpu 18
    node 0 socket 0 core 10 thread 0 cpu 20
    node 0 socket 0 core 10 thread 1 cpu 22
    node 0 socket 0 core 12 thread 0 cpu 24
    node 0 socket 0 core 12 thread 1 cpu 26
    node 0 socket 0 core 14 thread 0 cpu 28
    node 0 socket 0 core 14 thread 1 cpu 30
    node 2 socket 0 core 0 thread 0 cpu 1
    node 2 socket 0 core 0 thread 1 cpu 3
    node 2 socket 0 core 2 thread 0 cpu 5
    node 2 socket 0 core 2 thread 1 cpu 7
    node 2 socket 0 core 4 thread 0 cpu 9
    node 2 socket 0 core 4 thread 1 cpu 11
    node 2 socket 0 core 6 thread 0 cpu 13
    node 2 socket 0 core 6 thread 1 cpu 15
    node 2 socket 0 core 8 thread 0 cpu 17
    node 2 socket 0 core 8 thread 1 cpu 19
    node 2 socket 0 core 10 thread 0 cpu 21
    node 2 socket 0 core 10 thread 1 cpu 23
    node 2 socket 0 core 12 thread 0 cpu 25
    node 2 socket 0 core 12 thread 1 cpu 27
    node 2 socket 0 core 14 thread 0 cpu 29
    node 2 socket 0 core 14 thread 1 cpu 31
    
    Below is a quick fix which would correct this to
    
    molitor:~/:[0]# ./list-cpu-toplogy.sh
    node 0 socket 0 core 0 thread 0 cpu 0
    node 0 socket 0 core 0 thread 1 cpu 2
    node 0 socket 0 core 2 thread 0 cpu 4
    node 0 socket 0 core 2 thread 1 cpu 6
    node 0 socket 0 core 4 thread 0 cpu 8
    node 0 socket 0 core 4 thread 1 cpu 10
    node 0 socket 0 core 6 thread 0 cpu 12
    node 0 socket 0 core 6 thread 1 cpu 14
    node 1 socket 0 core 0 thread 0 cpu 16
    node 1 socket 0 core 0 thread 1 cpu 18
    node 1 socket 0 core 2 thread 0 cpu 20
    node 1 socket 0 core 2 thread 1 cpu 22
    node 1 socket 0 core 4 thread 0 cpu 24
    node 1 socket 0 core 4 thread 1 cpu 26
    node 1 socket 0 core 6 thread 0 cpu 28
    node 1 socket 0 core 6 thread 1 cpu 30
    node 2 socket 0 core 0 thread 0 cpu 1
    node 2 socket 0 core 0 thread 1 cpu 3
    node 2 socket 0 core 2 thread 0 cpu 5
    node 2 socket 0 core 2 thread 1 cpu 7
    node 2 socket 0 core 4 thread 0 cpu 9
    node 2 socket 0 core 4 thread 1 cpu 11
    node 2 socket 0 core 6 thread 0 cpu 13
    node 2 socket 0 core 6 thread 1 cpu 15
    node 3 socket 0 core 0 thread 0 cpu 17
    node 3 socket 0 core 0 thread 1 cpu 19
    node 3 socket 0 core 2 thread 0 cpu 21
    node 3 socket 0 core 2 thread 1 cpu 23
    node 3 socket 0 core 4 thread 0 cpu 25
    node 3 socket 0 core 4 thread 1 cpu 27
    node 3 socket 0 core 6 thread 0 cpu 29
    node 3 socket 0 core 6 thread 1 cpu 31
    
    But I'll provide a different patch to really fix this as the scripts
    take considerable time to gather this info on molitor.
    
    Signed-off-by: Andreas Herrmann <aherrmann@suse.com>

--- bin/list-cpu-siblings.pl
+++ bin/list-cpu-siblings.pl
@@ -3,8 +3,10 @@
 use strict;
 die if !defined $ARGV[0];
 die if !defined $ARGV[1];
+die if !defined $ARGV[2];
 
 my @ht_list;
+my @node_core_list;
 my @sibling_list;
 
 open(INPUT, "/sys/bus/cpu/drivers/processor/cpu$ARGV[0]/topology/thread_siblings_list") ||
@@ -32,6 +34,30 @@ while (!eof(INPUT)) {
 	}
 }
 close INPUT;
+
+open(INPUT, "/sys/bus/cpu/drivers/processor/cpu$ARGV[0]/node$ARGV[2]/cpulist") ||
+	open(INPUT, "/sys/devices/system/cpu/cpu$ARGV[0]/node$ARGV[2]/cpulist") ||
+	die("Failed to open node cpulist file for CPU $ARGV[0]");
+while (!eof(INPUT)) {
+	my $line = <INPUT>;
+
+	foreach my $range (split /,/, $line) {
+		chomp($range);
+		if ($line =~ /-/) {
+			my ($from, $to) = split(/-/, $range);
+			for (my $i = $from; $i <= $to; $i++) {
+				if ($i == $ARGV[0] || grep {$_ == $i} @ht_list) {
+					next;
+				}
+				push @node_core_list, $i;
+			}
+		} else {
+			if ($range != $ARGV[0] || grep {$_ == $range} @ht_list) {
+				push @node_core_list, $range;
+			}
+		}
+	}
+}
 
 open(INPUT, "/sys/bus/cpu/drivers/processor/cpu$ARGV[0]/topology/core_siblings_list") ||
 	open(INPUT, "/sys/devices/system/cpu/cpu$ARGV[0]/topology/core_siblings_list") ||
@@ -61,6 +87,8 @@ while (!eof(INPUT)) {
 
 if ($ARGV[1] eq "threads") {
 	print join ",", @ht_list;
+} elsif ($ARGV[1] eq "node_cores") {
+	print join ",", @node_core_list;
 } elsif ($ARGV[1] eq "cores") {
 	print join ",", @sibling_list;
 } else {
--- bin/list-cpu-toplogy.sh
+++ bin/list-cpu-toplogy.sh
@@ -17,14 +17,14 @@ for NODE in $NODES; do
 	CPUS=`numactl --hardware | grep "node $NODE cpus:" | awk -F : '{print $2}' | sed -e 's/^\s*//'`
 	for CPU in $CPUS; do
 		CORE=0
-		for CORE_SIBLING in $CPU `$SCRIPTDIR/list-cpu-siblings.pl $CPU cores | sed -e 's/,/ /g'`; do
+		for CORE_SIBLING in $CPU `$SCRIPTDIR/list-cpu-siblings.pl $CPU node_cores $NODE | sed -e 's/,/ /g'`; do
 			THREAD=0
 			if [ "${SEEN[$CORE_SIBLING]}" != "yes" ]; then
 				echo node $NODE socket $SOCKET core $CORE thread $THREAD cpu $CORE_SIBLING
 				SEEN[$CORE_SIBLING]=yes
 			fi
 
-			for THREAD_SIBLING in `$SCRIPTDIR/list-cpu-siblings.pl $CORE_SIBLING threads | sed -e 's/,/ /g'`; do
+			for THREAD_SIBLING in `$SCRIPTDIR/list-cpu-siblings.pl $CORE_SIBLING threads $NODE | sed -e 's/,/ /g'`; do
 				if [ "${SEEN[$THREAD_SIBLING]}" != "yes" ]; then
 					THREAD=$((THREAD+1))
 					echo node $NODE socket $SOCKET core $CORE thread $THREAD cpu $THREAD_SIBLING
