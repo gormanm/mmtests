0b42f1128639c3ff66f803665e5dc3ed42a7c6c6
Mel Gorman <mgorman@techsingularity.net>
2018-10-09 11:42:15 +0100

    watch-top: Filter by kswapd by default as that is all compare-kernels.sh consumes
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- monitors/watch-top.sh
+++ monitors/watch-top.sh
@@ -2,15 +2,22 @@
 if [ "$MONITOR_TOP_ACTIVEONLY" = "" ]; then
 	MONITOR_TOP_ACTIVEONLY=yes
 fi
+FILTERPIDS=
+if [ "$MONITOR_TOP_KSWAPDONLY" = "" -o "$MONITOR_TOP_KSWAPD_ONLY" = "yes" ]; then
+	for KSWAPD_PID in `ps auxw | grep -E '\[kswapd[0-9]*\]' | awk '{print $2}'`; do
+		FILTERPIDS="$FILTERPIDS -p $KSWAPD_PID"
+	done
+fi
+
 if [ "$MONITOR_TOP_ACTIVEONLY" != "yes" ]; then
-	exec top -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
+	exec top $FILTERPIDS -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
 		if ($_ =~ /^top -.*/) {
 			print "time: " . time . " full\n";
 		}
 		print $_;
 	}'
 else
-	exec top -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
+	exec top $FILTERPIDS -b -d $MONITOR_UPDATE_FREQUENCY | perl -e 'while (<>) {
 		if ($_ =~ /^top -.*/) {
 			print "time: " . time . " short\n";
 		}
