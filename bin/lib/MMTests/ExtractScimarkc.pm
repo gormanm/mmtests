908693a3441380323cc12f4b8c39641fc2f35c1c
Mel Gorman <mgorman@techsingularity.net>
2018-10-18 16:34:19 +0100

    scimark: Add scimark C benchmark
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

---
+++ bin/lib/MMTests/ExtractScimarkc.pm
@@ -0,0 +1,46 @@
+# ExtractScimarkc.pm
+package MMTests::ExtractScimarkc;
+use MMTests::SummariseMultiops;
+our @ISA = qw(MMTests::SummariseMultiops);
+use strict;
+
+sub initialise() {
+	my ($self, $reportDir, $testName) = @_;
+	$self->{_ModuleName} = "ExtractScimarkc";
+	$self->{_DataType}   = DataTypes::DATA_OPS_PER_MINUTE;
+	$self->{_PlotType}   = "histogram";
+	$self->{_ClientSubheading} = 1;
+
+	$self->SUPER::initialise($reportDir, $testName);
+}
+
+sub extractReport() {
+	my ($self, $reportDir, $reportName, $profile) = @_;
+	my %opNames;
+	my $iteration = 0;
+
+	foreach my $file (<$reportDir/$profile/scimarkc.*>) {
+		open(INPUT, $file) || die("Failed to open $file\n");
+		$iteration++;
+		while (<INPUT>) {
+			my $line = $_;
+			next if $line !~ /Mflops:/;
+
+			my @elements = split(/\s+/, $line);
+			$elements[0] =~ s/ /_/;
+
+			$opNames{$elements[0]} = 1;
+			push @{$self->{_ResultData}}, [ "$elements[0]", $iteration, $elements[2] ];
+		}
+		close(INPUT);
+	}
+
+	my @ops;
+	my @opNameSorted = sort keys %opNames;
+	foreach my $opName (@opNameSorted) {
+		push @ops, "$opName";
+	}
+	$self->{_Operations} = \@ops;
+}
+
+1;
