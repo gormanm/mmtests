4281222edd126cf61aa4bfa5f7065049633618d1
Mel Gorman <mgorman@techsingularity.net>
2018-10-31 15:17:22 +0000

    perfpipe: Add benchmark for perf sched pipe
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

---
+++ drivers/driver-perfpipe.sh
@@ -0,0 +1,18 @@
+FINEGRAINED_SUPPORTED=yes
+NAMEEXTRA=
+
+run_bench() {
+	VERSION_PARAM=
+	if [ "$PERFPIPE_VERSION" != "" ]; then
+		VERSION_PARAM="-v $PERFPIPE_VERSION"
+	fi
+	BIND_SWITCH=
+	if [ "$PERFPIPE_BINDING" != "" ]; then
+		BIND_SWITCH=--bind-$PERFPIPE_BINDING
+	fi
+
+	$SHELLPACK_INCLUDE/shellpack-bench-perfpipe $VERSION_PARAM $BIND_SWITCH	\
+		--loops		$PERFPIPE_LOOPS					\
+		--iterations	$PERFPIPE_ITERATIONS
+	return $?
+}
