10c93dbb8ecad2c33b8f7fd5010e38c97ed091df
Mel Gorman <mgorman@techsingularity.net>
2018-11-01 07:56:25 +0000

    perfsyscall: Add a perf-based syscall microbenchmark
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

---
+++ bin/lib/MMTests/ExtractPerfsyscall.pm
@@ -0,0 +1,40 @@
+# ExtractPerfsyscall.pm
+package MMTests::ExtractPerfsyscall;
+use MMTests::SummariseVariabletime;
+use VMR::Report;
+our @ISA = qw(MMTests::SummariseVariabletime);
+
+sub new() {
+	my $class = shift;
+	my $self = {
+		_ModuleName  => "ExtractPerfsyscall",
+		_DataType    => DataTypes::DATA_TIME_USECONDS,
+		_ResultData  => [],
+		_PlotType    => "simple",
+		_Precision   => 4,
+	};
+	bless $self, $class;
+	return $self;
+}
+
+sub extractReport() {
+	my ($self, $reportDir, $reportName, $profile) = @_;
+	my $iteration = 0;
+
+	foreach my $file (<$reportDir/$profile/syscall-*.log>) {
+		open(INPUT, $file) || die("Failed to open $file\n");
+		while (!eof(INPUT)) {
+			my $line = <INPUT>;
+			$line =~ s/^\s+|\s+$//g;
+
+			my @elements = split(/\s/, $line);
+			next if ($elements[1] ne "usecs/op");
+			push @{$self->{_ResultData}}, ["Time", ++$iteration, $elements[0]];
+		}
+		close(INPUT);
+	}
+
+	$self->{_Operations} = [ "Time" ];
+	close INPUT;
+}
+1;
---
+++ drivers/driver-perfsyscall.sh
@@ -0,0 +1,18 @@
+FINEGRAINED_SUPPORTED=yes
+NAMEEXTRA=
+
+run_bench() {
+	VERSION_PARAM=
+	if [ "$PERFSYSCALL_VERSION" != "" ]; then
+		VERSION_PARAM="-v $PERFSYSCALL_VERSION"
+	fi
+	BIND_SWITCH=
+	if [ "$PERFSYSCALL_BINDING" != "" ]; then
+		BIND_SWITCH=--bind-$PERFSYSCALL_BINDING
+	fi
+
+	$SHELLPACK_INCLUDE/shellpack-bench-perfsyscall $VERSION_PARAM $BIND_SWITCH	\
+		--loops		$PERFSYSCALL_LOOPS					\
+		--iterations	$PERFSYSCALL_ITERATIONS
+	return $?
+}
