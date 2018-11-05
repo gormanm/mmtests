8b4bd9e9a287cbbf1ac508d4e664ed6eff6281ca
Mel Gorman <mgorman@techsingularity.net>
2018-11-05 21:50:56 +0000

    watch-ftrace: Optionally enable global tracing options
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- monitors/watch-ftrace.sh
+++ monitors/watch-ftrace.sh
@@ -4,6 +4,12 @@ if [ ! -e /sys/kernel/debug/tracing/ ]; then
 	mount -t debugfs none /sys/kernel/debug 2>&1 || exit -1
 fi
 
+for OPTION in $MONITOR_FTRACE_OPTIONS; do
+	if [ -e /sys/kernel/debug/tracing/options/$OPTION ]; then
+		echo 1 > /sys/kernel/debug/tracing/options/$OPTION
+	fi
+done
+
 for EVENT in $MONITOR_FTRACE_EVENTS; do
 	SUBSYSTEM=`echo $EVENT | awk -F / '{print $1}'`
 	if [ "$SUBSYSTEM" = "probe_func_return" ]; then
