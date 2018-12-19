457e64cdb488463d279cb742a6d69c84b84ec969
Jan Kara <jack@suse.cz>
2018-12-19 11:27:27 +0100

    MonitorNumanodeusage: Use standard _ResultData format
    Use the same format for _ResultData as consumed by SummariseMultiops.
    This will allow generic support for multiple iterations and also
    simplifies the code.
    
    Signed-off-by: Jan Kara <jack@suse.cz>

--- bin/lib/MMTests/MonitorNumanodeusage.pm
+++ bin/lib/MMTests/MonitorNumanodeusage.pm
@@ -11,7 +11,8 @@ sub new() {
 	my $self = {
 		_ModuleName  => "MonitorDuration",
 		_DataType    => MMTests::Monitor::MONITOR_NUMA_USAGE,
-		_ResultData  => []
+		_ResultData  => [],
+		_MultiopMonitor => 1
 	};
 	bless $self, $class;
 	return $self;
@@ -33,7 +34,6 @@ sub printDataType() {
 
 sub extractReport($$$$$) {
 	my ($self, $reportDir, $testName, $testBenchmark, $subHeading) = @_;
-	my $vmstat = "";
 	my $timestamp = 0;
 	my $start_timestamp = 0;
 
@@ -51,61 +51,51 @@ sub extractReport($$$$$) {
 		open(INPUT, "gunzip -c $file|") || die("Failed to open $file: $!\n");
 	}
 
-	my $buildingHeader = 1;
-	my @row;
+	if ($subHeading eq "Usage") {
+		$self->{_FieldHeaders} = [ "Node", "Time", "Bytes" ];
+		$self->{_FieldFormat} = ["%${fieldLength}s", "%${fieldLength}d",
+				 "%${fieldLength}d" ];
+	} else {
+		$self->{_FieldHeaders} = [ "Node", "Time", "MemoryBalance" ];
+		$self->{_FieldFormat} = ["%${fieldLength}s", "%${fieldLength}d",
+				 "%${fieldLength}.6f" ];
+	}
+
 	my @nodeUsage;
 	my @nodeSize;
-	my @header;
-	my @format;
-	my $fieldLength = $self->{_FieldLength};
 	while (<INPUT>) {
 		if ($_ =~ /^time: ([0-9]+)/) {
 			if ($start_timestamp == 0) {
 				$start_timestamp = $1;
-				push @header, "Time";
-				push @format, "%${fieldLength}d";
+				$timestamp = $1;
+				next;
 			}
-			$timestamp = $1;
-			if ($#nodeUsage > 0) {
-				if ($subHeading eq "Usage") {
-					push @{$self->{_ResultData}}, [ ( @row, @nodeUsage ) ];
-				} else {
-					my @tuples;
-					for (my $i = 0; $i <= $#nodeUsage; $i++) {
-						push @tuples, $nodeUsage[$i];
-						push @tuples, $nodeSize[$i];
-					}
-					push @{$self->{_ResultData}}, [ ( @row, numa_memory_balance(@tuples) ) ];
+			if ($subHeading eq "MemoryBalance") {
+				my @tuples;
+				for (my $i = 0; $i <= $#nodeUsage; $i++) {
+					push @tuples, $nodeUsage[$i];
+					push @tuples, $nodeSize[$i];
 				}
+				push @{$self->{_ResultData}}, [ "Balance", $timestamp - $start_timestamp, numa_memory_balance(@tuples) ];
 
 				$#nodeUsage = -1;
 				$#nodeSize  = -1;
-				$#row = -1;
-				$buildingHeader = 0;
 			}
-			push @row, ($timestamp - $start_timestamp);
+			$timestamp = $1;
+
 			next;
 		}
 		if ($_ =~ /^Node ([0-9]+) MemUsed:\s+([0-9]*)/) {
-			push @nodeUsage, $2 * 1024;
-			if ($buildingHeader) {
-				if ($subHeading eq "Usage") {
-					push @header, "Node$1";
-					push @format, "%${fieldLength}d";
-				} else {
-					push @header, "MemoryBalance";
-					push @format, "%${fieldLength}.6f";
-					$buildingHeader = 0;
-				}
+			if ($subHeading eq "Usage") {
+				push @{$self->{_ResultData}}, [ "Node$1", $timestamp - $start_timestamp, $2 * 1024 ];
+			} else {
+				push @nodeUsage, $2 * 1024;
 			}
-		}
-		if ($_ =~ /^Node ([0-9]+) MemTotal:\s+([0-9]*)/) {
-			push @nodeSize, $2 * 1024;
+		} elsif ($subHeading eq "MemoryBalance" &&
+			 $_ =~ /^Node [0-9]+ MemTotal:\s+([0-9]*)/) {
+			push @nodeSize, $1 * 1024;
 		}
 	}
-
-	$self->{_FieldHeaders} = [ @header ];
-	$self->{_FieldFormat} = [ @format ];
 }
 
 1;
