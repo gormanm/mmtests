6c0c951a37c3669223b21e73b673fbd4eb3919dc
Mel Gorman <mgorman@techsingularity.net>
2019-04-10 11:27:39 +0100

    ltp: Use LTP logging helpers
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-ltp.sh
+++ drivers/driver-ltp.sh
@@ -6,12 +6,8 @@ run_bench() {
 	if [ "$LTP_RUN_ARGS" != "" ]; then
 		LTP_RUN_ARGS_SWITCH="--ltp-args \"$LTP_RUN_ARGS\""
 	fi
-	LTP_RUN_ITERATIONS_SWITCH=
-	if [ "$LTP_RUN_ITERATIONS" != "" ]; then
-		LTP_RUN_ITERATIONS_SWITCH="--ltp-iterations \"$LTP_RUN_ITERATIONS\""
-	fi
 
 	eval $SCRIPTDIR/shellpacks/shellpack-bench-ltp \
-			--ltp-tests "$LTP_RUN_TESTS" $LTP_RUN_ARGS_SWITCH $LTP_RUN_ITERATIONS_SWITCH
+			--ltp-tests "$LTP_RUN_TESTS" $LTP_RUN_ARGS_SWITCH
 	return $?
 }
