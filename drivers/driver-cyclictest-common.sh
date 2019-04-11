d04686524d66e597c7ae2b77eb0a52ccd65c9abf
Mel Gorman <mgorman@techsingularity.net>
2019-04-11 18:15:29 +0100

    cyclictest: Wire up background parameter passing
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-cyclictest-common.sh
+++ drivers/driver-cyclictest-common.sh
@@ -5,7 +5,11 @@ run_bench() {
 	if [ "$CYCLICTEST_PINNED" = "yes" ]; then
 		CYCLICTEST_PINNED_PARAM="--affinity"
 	fi
-	$SHELLPACK_INCLUDE/shellpack-bench-cyclictest $CYCLICTEST_PINNED_PARAM \
+	CYCLICTEST_BACKGROUND_PARAM=
+	if [ "$CYCLICTEST_BACKGROUND" != "" ]; then
+		CYCLICTEST_BACKGROUND_PARAM="--background $CYCLICTEST_BACKGROUND"
+	fi
+	$SHELLPACK_INCLUDE/shellpack-bench-cyclictest $CYCLICTEST_PINNED_PARAM $CYCLICTEST_BACKGROUND_PARAM \
 		--iterations $CYCLICTEST_ITERATIONS    \
 		--duration   $CYCLICTEST_DURATION
 	return $?
