aa7b063ee967cac66649108ab113b9706872d432
Mel Gorman <mgorman@techsingularity.net>
2019-04-11 15:52:16 +0100

    pistress: Remove multiple iterations support
    Multiple iterations is not relevant for a pure functional test. Simply
    run one iteration for longer.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-pistress.sh
+++ drivers/driver-pistress.sh
@@ -5,7 +5,6 @@ run_bench() {
 	$SHELLPACK_INCLUDE/shellpack-bench-pistress \
 		--min-invgroups $PISTRESS_MIN_INVGROUPS \
 		--max-invgroups $PISTRESS_MAX_INVGROUPS \
-		--iterations $PISTRESS_ITERATIONS \
 		--runtime $PISTRESS_RUNTIME
 	return $?
 }
