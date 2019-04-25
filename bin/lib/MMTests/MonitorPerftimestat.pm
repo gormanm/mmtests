f7d54a820bde7f16d4666d08fbea4a8f40269775
Jan Kara <jack@suse.cz>
2019-04-25 16:00:05 +0200

    MonitorPerftimestat: Convert to SummariseMonitor
    Convert MonitorPerftimestat to use SummariseMonitor for summarization.
    
    Signed-off-by: Jan Kara <jack@suse.cz>

--- bin/lib/MMTests/MonitorPerftimestat.pm
+++ bin/lib/MMTests/MonitorPerftimestat.pm
@@ -1,72 +1,39 @@
 # MonitorPerftimestat.pm
 package MMTests::MonitorPerftimestat;
-use MMTests::Monitor;
+use MMTests::SummariseMonitor;
 use MMTests::Stat;
-our @ISA = qw(MMTests::Monitor);
+our @ISA = qw(MMTests::SummariseMonitor);
 use strict;
 
 sub new() {
 	my $class = shift;
 	my $self = {
 		_ModuleName    => "MonitorPerftimestat",
-		_DataType      => MMTests::Monitor::MONITOR_PERFTIMESTAT,
-		_MultiopMonitor => 1,
 	};
 	bless $self, $class;
 	return $self;
 }
 
+use constant headingnames => {
+	"cpu-migrations"	=> "CPU Migrations",
+	"context-switches"	=> "Context Switches",
+	"task-clock"		=> "Task Clock",
+	"page-faults"		=> "Page Faults",
+	"cycles"		=> "Cycles",
+	"instructions"		=> "Instructions",
+	"branches"		=> "Branches",
+	"branch-misses"		=> "Branch Misses",
+};
+
 sub initialise() {
 	my ($self, $reportDir, $testName) = @_;
 
 	$self->{_FieldLength}   = 18;
-	$self->{_TestName}      = $testName;
+	$self->{_ExactSubheading} = 1;
+	$self->{_DataType} = DataTypes::DATA_ACTIONS;
+	$self->{_PlotXaxis} = "Time";
+	$self->{_PlotYaxes} = $headingnames;
 	$self->SUPER::initialise($reportDir, $testName);
-}
-
-sub printDataType() {
-	my ($self, $subHeading) = @_;
-
-	if ($subHeading eq "cpu-migrations") {
-		print "CPU Migrations,Events,Migrations\n";
-	} elsif ($subHeading eq "context-switches") {
-		print "Context Switches,Time,Context Switches\n";
-	} elsif ($subHeading eq "task-clock") {
-		print "Task Clock,Time,Task Clock\n";
-	} elsif ($subHeading eq "page-faults") {
-		print "Page Faults,Events,Page Faults\n";
-	} elsif ($subHeading eq "cycles") {
-		print "Cycles,Events,Cycles\n";
-	} elsif ($subHeading eq "instructions") {
-		print "Instructions,Events,Instructions\n";
-	} elsif ($subHeading eq "branches") {
-		print "Branches,Events,Branches\n";
-	} elsif ($subHeading eq "branch-misses") {
-		print "Branch Misses,Events,Branch Misses";
-	}
-}
-
-sub extractSummary() {
-	my ($self, $subheading) = @_;
-	my %data = %{$self->dataByOperation()};
-
-	my $fieldLength = 18;
-	$self->{_SummaryHeaders} = [ "Statistic", "Mean", "Max" ];
-	$self->{_FieldFormat} = [ "%${fieldLength}s", "%${fieldLength}.2f", ];
-
-	foreach my $header (keys %data) {
-		my @samples = map {$_ -> [1]} @{$data{$header}};
-
-		push @{$self->{_SummaryData}}, [ "$header",
-			 calc_amean(\@samples), calc_max(\@samples) ];
-	}
-
-	return 1;
-}
-
-sub printPlot() {
-	my ($self) = @_;
-	$self->{_PrintHandler}->printRow($self->{_ResultData}, $self->{_FieldLength}, $self->{_FieldFormat});
 }
 
 sub extractReport($$$$) {
@@ -82,9 +49,6 @@ sub extractReport($$$$) {
 		$file .= ".gz";
 		open(INPUT, "gunzip -c $file|") || die("Failed to open $file: $!\n");
 	}
-
-
-	$self->{_FieldHeaders}  = [ "Op", "Time", "Value" ];
 
 	# Read all counters
 	my $timestamp;
