8ba1fc2ad2e945baba7eb7a05a6033db6bb4f811
Mel Gorman <mgorman@techsingularity.net>
2019-05-23 14:29:36 +0100

    Update version of perf used for benchmarks
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-vmscale.sh
+++ drivers/driver-vmscale.sh
@@ -1,11 +1,7 @@
 NAMEEXTRA=
 
 run_bench() {
-	USEPERF_PARAM=
-	if [ "$VMSCALE_USE_PERF" = "yes" ]; then
-		USEPERF_PARAM=--use-perf
-	fi
-	$SHELLPACK_INCLUDE/shellpack-bench-vmscale $USEPERF_PARAM \
+	$SHELLPACK_INCLUDE/shellpack-bench-vmscale \
 		--cases "$VMSCALE_CASES"
 	return $?
 }
