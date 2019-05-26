fc5cd9b2bc32ea4843a9dc7c1ee216678f0cd143
Mel Gorman <mgorman@techsingularity.net>
2019-05-26 10:55:08 +0100

    dedup: Simplify report extraction
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/lib/MMTests/ExtractDedup.pm
+++ bin/lib/MMTests/ExtractDedup.pm
@@ -20,32 +20,14 @@ sub extractReport() {
 	my ($tp, $name);
 	my @threads;
 
-	my @files = <$reportDir/dedup-*-1.time>;
-	foreach my $file (@files) {
-		my @elements = split (/-/, $file);
-		my $thr = $elements[-2];
-		$thr =~ s/.log//;
-		push @threads, $thr;
-	}
-	@threads = sort { $a <=> $b } @threads;
+	@threads = $self->discover_scaling_parameters($reportDir, "dedup-", "-1.time");
 
 	foreach my $nthr (@threads) {
 		my @files = <$reportDir/dedup-$nthr-*.time>;
+		my $iteration = 0;
 
 		foreach my $file (@files) {
-			my @split = split /-/, $file;
-			$split[-1] =~ s/.time//;
-			my $nr_samples = 0;
-
-			open(INPUT, $file) || die("Failed to open $file\n");
-			while (<INPUT>) {
-				my $line = $_;
-				if ($line =~ /([0-9]):([0-9.]+)elapsed/) {
-					$self->addData($nthr, ++$nr_samples, $self->_time_to_elapsed($line));
-				}
-			}
-			close INPUT;
+			$self->parse_time_elapsed($file, $nthr, ++$iteration);
 		}
-
 	}
 }
