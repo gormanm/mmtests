gormanm/mmtests

commit 33e0a0c68ab31a47e03aa07a0147a950e9041221

Extract: Use automatic generation of _Operations where possible
For lot of benchmarks, we can just use the automated generation of
_Operations by the Summarise module. So drop unnecessary computation of
_Operations.

Signed-off-by: Jan Kara <jack@suse.cz>

bin/lib/MMTests/ExtractSstartup.pm
@@ -30,7 +30,6 @@ sub extractReport($$$) {
 	}
 	@jobnames = sort { $a <=> $b } @jobnames;
 
-	my %jobnamesPatterns;
 	foreach my $jobname (@jobnames) {
 		my $reading = 0;
 
@@ -55,8 +54,6 @@ sub extractReport($$$) {
 			# at the moment).
 			foreach my $pattern (@patterns) {
 				my $nr_samples=0;
-				my $jobnamePattern="$jobname-$pattern";
-				$jobnamesPatterns{$jobnamePattern} = 0;
 				open INPUT,
 				"$reportDir/results/replayed_$jobname\_startup/repetition0/$scheduler-$pattern-single_times.txt"
 				|| die "Failed to find time data file
@@ -66,7 +63,6 @@ sub extractReport($$$) {
 					my $line = <INPUT>;
 					chomp($line);
 					$line =~ s/^\s+//;
-					$jobnamesPatterns{$jobnamePattern} = 1;
 					$nr_samples++;
 					$self->addData("$jobnamePattern", $nr_samples, $line);
 				}
@@ -74,11 +70,5 @@ sub extractReport($$$) {
 			}
 		}
 	}
-
-	my @ops;
-	foreach my $jobnamePattern (sort keys %jobnamesPatterns) {
-		push @ops, "$jobnamePattern";
-	}
-	$self->{_Operations} = \@ops;
 }
 1;

commit 5b661c613e8bf02bcf2a4747587333bef4a84b07

ExtractSstartup: Fix generation of op when adding data
This was temporarily broken by commit
33e0a0c6 ("Extract: Use automatic generation of _Operations where possible")
which resulted in following error when ExtractSstartup.pm was loaded:

  Global symbol "$jobnamePattern" requires explicit package name at [...]/ExtractSstartup.pm line 67.

Signed-off-by: Andreas Herrmann <aherrmann@suse.com>
Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

bin/lib/MMTests/ExtractSstartup.pm
@@ -64,7 +64,7 @@ sub extractReport($$$) {
 					chomp($line);
 					$line =~ s/^\s+//;
 					$nr_samples++;
-					$self->addData("$jobnamePattern", $nr_samples, $line);
+					$self->addData("$jobname-$pattern", $nr_samples, $line);
 				}
 				close INPUT;
 			}