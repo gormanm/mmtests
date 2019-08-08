283644c4a37650e72b9667eebef2745a0b3392c1
Mel Gorman <mgorman@techsingularity.net>
2019-08-08 09:44:34 +0100

    fsmark: Adjust fs_mark small file stream configuration to report once sample per iteration
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-fsmark-single.sh
+++ drivers/driver-fsmark-single.sh
@@ -1,11 +1,13 @@
 $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh fsmark
 
 run_bench() {
+	FSMARK_NR_TOP_DIRECTORIES=${FSMARK_NR_TOP_DIRECTORIES:-1}
 	$SHELLPACK_INCLUDE/shellpack-bench-fsmark			\
 		--min-threads 1						\
 		--max-threads 1						\
 		--filesize $FSMARK_FILESIZE				\
-		--nr-files-per-iteration $FSMARK_NR_FILES_PER_ITERATION \
+		--nr-files-per-iteration $FSMARK_NR_FILES_PER_ITERATION	\
+		--nr-top-directories $FSMARK_NR_TOP_DIRECTORIES		\
 		--nr-sub-directories $FSMARK_NR_SUB_DIRECTORIES		\
 		--iterations $FSMARK_ITERATIONS
 	return $?
--- drivers/driver-fsmark-threaded.sh
+++ drivers/driver-fsmark-threaded.sh
@@ -1,12 +1,14 @@
 $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh fsmark
 
 run_bench() {
-	$SHELLPACK_INCLUDE/shellpack-bench-fsmark \
-		--min-threads $FSMARK_MIN_THREADS \
-		--max-threads $FSMARK_MAX_THREADS \
-		--filesize $FSMARK_FILESIZE \
-		--nr-files-per-iteration $FSMARK_NR_FILES_PER_ITERATION \
-		--nr-sub-directories $FSMARK_NR_SUB_DIRECTORIES \
+	FSMARK_NR_TOP_DIRECTORIES=${FSMARK_NR_TOP_DIRECTORIES:-1}
+	$SHELLPACK_INCLUDE/shellpack-bench-fsmark			\
+		--min-threads $FSMARK_MIN_THREADS			\
+		--max-threads $FSMARK_MAX_THREADS			\
+		--filesize $FSMARK_FILESIZE				\
+		--nr-files-per-iteration $FSMARK_NR_FILES_PER_ITERATION	\
+		--nr-top-directories $FSMARK_NR_TOP_DIRECTORIES		\
+		--nr-sub-directories $FSMARK_NR_SUB_DIRECTORIES		\
 		--iterations $FSMARK_ITERATIONS
 	return $?
 }
