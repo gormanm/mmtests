5017e4c8ccd1f67b0e949c94d596f81f924a4577
Mel Gorman <mgorman@techsingularity.net>
2019-10-17 14:57:07 +0100

    kernbench: Allow the warmup step to be skipped
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-kernbench.sh
+++ drivers/driver-kernbench.sh
@@ -2,14 +2,18 @@
 run_bench() {
 	CONFIG_SWITCH=
 	TARGET_SWITCH=
+	SKIP_SWITCH=
 	if [ "$KERNBENCH_CONFIG" != "" ]; then
 		CONFIG_SWITCH="--kernel-config $KERNBENCH_CONFIG"
 	fi
 	if [ "$KERNBENCH_TARGETS" != "" ]; then
 		TARGET_SWITCH="--build-targets $KERNBENCH_TARGETS"
 	fi
+	if [ "$KERNBENCH_SKIP_WARMUP" = "yes" ]; then
+		SKIP_SWITCH="--skip-warmup"
+	fi
 
-	$SCRIPTDIR/shellpacks/shellpack-bench-kernbench $CONFIG_SWITCH $TARGET_SWITCH \
+	$SCRIPTDIR/shellpacks/shellpack-bench-kernbench $CONFIG_SWITCH $TARGET_SWITCH $SKIP_SWITCH \
 		--min-threads $KERNBENCH_MIN_THREADS \
 		--max-threads $KERNBENCH_MAX_THREADS \
 		--iterations $KERNBENCH_ITERATIONS
