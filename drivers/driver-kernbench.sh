8c91d735145bb4be105aa2a3f43056a649f50986
Mel Gorman <mgorman@techsingularity.net>
2019-10-17 14:39:52 +0100

    kernbench: Add configuration that builds allmodconfig with all CPUs only
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-kernbench.sh
+++ drivers/driver-kernbench.sh
@@ -1,6 +1,15 @@
 
 run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-kernbench \
+	CONFIG_SWITCH=
+	TARGET_SWITCH=
+	if [ "$KERNBENCH_CONFIG" != "" ]; then
+		CONFIG_SWITCH="--kernel-config $KERNBENCH_CONFIG"
+	fi
+	if [ "$KERNBENCH_TARGETS" != "" ]; then
+		TARGET_SWITCH="--build-targets $KERNBENCH_TARGETS"
+	fi
+
+	$SCRIPTDIR/shellpacks/shellpack-bench-kernbench $CONFIG_SWITCH $TARGET_SWITCH \
 		--min-threads $KERNBENCH_MIN_THREADS \
 		--max-threads $KERNBENCH_MAX_THREADS \
 		--iterations $KERNBENCH_ITERATIONS
