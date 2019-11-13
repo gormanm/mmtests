770de47d2d63369964f76622076c0fd555c07907
Andreas Herrmann <aherrmann@suse.com>
2019-11-13 09:17:29 +0000

    bonnie++: Add better control for char I/O tests
    Patch bonnie++ sources to fix '-f' option argument parsing and to add
    new '-F' option.  Update bonnie++ shellpack to control size of char
    I/O tests. This will be done using variable BONNIE_CHAR_IO_SIZE in
    corresponding config files. Default (if this config variable is not
    specified) is to skip char I/O tests.
    
    Signed-off-by: Andreas Herrmann <aherrmann@suse.com>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-bonnie++.sh
+++ drivers/driver-bonnie++.sh
@@ -13,9 +13,14 @@ run_bench() {
 		SIZE_PARAM="--dataset $BONNIE_DATASET_SIZE"
 	fi
 
+	if [ "$BONNIE_CHAR_IO_SIZE" != "" ]; then
+		CHAR_IO_PARAM="--char_io_size $BONNIE_CHAR_IO_SIZE"
+	fi
+
 	$SHELLPACK_INCLUDE/shellpack-bench-bonnie++ $BONNIE_FSYNC_PARAM \
 		$SIZE_PARAM \
 		$FILES_PARAM \
-		$DIRS_PARAM
+		$DIRS_PARAM \
+		$CHAR_IO_PARAM
 	return $?
 }
