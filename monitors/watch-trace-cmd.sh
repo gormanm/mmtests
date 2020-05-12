119916d575891d1a77dd55a3d14aa562057ec17e
Mel Gorman <mgorman@techsingularity.net>
2020-05-12 13:40:47 +0100

    watch-trace-cmd: Allow plugin to be specified
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- monitors/watch-trace-cmd.sh
+++ monitors/watch-trace-cmd.sh
@@ -6,10 +6,14 @@ fi
 
 install-depends trace-cmd
 
-TRACE_EVENTS=
+TRACE_PLUGIN=
+if [ "$MONITOR_FTRACE_PLUGIN" != "" ]; then
+	TRACE_PLUGIN="-p $MONITOR_FTRACE_PLUGIN"
+fi
 
+TRACE_EVENTS=
 for EVENT in $MONITOR_FTRACE_EVENTS; do
 	TRACE_EVENTS="$TRACE_EVENTS -e $EVENT"
 done
 
-exec trace-cmd record $TRACE_EVENTS -o $MONITOR_LOG-trace.dat
+exec trace-cmd record $TRACE_PLUGIN $TRACE_EVENTS -o $MONITOR_LOG-trace.dat
