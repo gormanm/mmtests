5074dc97fc1bbd1a1bfebef41db926339a862be0
mgorman <mgorman@suse.com>
2020-07-29 11:15:51 +0100

    specfem3d: Rebuild openmpi if alternative toolchain is used
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- shellpack_src/src/refresh.sh
+++ shellpack_src/src/refresh.sh
@@ -13,14 +13,20 @@ if [ ! -d $1 ]; then
 	echo Package must be a directory
 	exit -1
 fi
+if [ -e $1/$1-bench ]; then
+	cp $1/$1-bench ../../shellpacks/shellpack-bench-$1
+fi
+cp $1/$1-install ../../shellpacks/shellpack-install-$1
 
-if [ -e $1/$1-bench ]; then
-	cat $1/$1-bench   | ../bin/rewrite-shellpack $1 > ../../shellpacks/shellpack-bench-$1
-fi
-cat $1/$1-install | ../bin/rewrite-shellpack $1 > ../../shellpacks/shellpack-install-$1
-if [ -e $1/$1-bench ]; then
-	chmod a+x ../../shellpacks/shellpack-bench-$1
-fi
-chmod a+x ../../shellpacks/shellpack-install-$1
+for STAGES in 1 2; do
+	if [ -e ../../shellpacks/shellpack-bench-$1 ]; then
+		cat ../../shellpacks/shellpack-bench-$1 | ../bin/rewrite-shellpack $1 > ../../shellpacks/shellpack-bench-$1.stage
+		mv ../../shellpacks/shellpack-bench-$1.stage ../../shellpacks/shellpack-bench-$1
+		chmod a+x ../../shellpacks/shellpack-bench-$1
+	fi
+	cat ../../shellpacks/shellpack-install-$1 | ../bin/rewrite-shellpack $1 > ../../shellpacks/shellpack-install-$1.stage
+	mv ../../shellpacks/shellpack-install-$1.stage ../../shellpacks/shellpack-install-$1
+	chmod a+x ../../shellpacks/shellpack-install-$1
+done
 
 exit 0
