b3c60cfb0ef445e33f9f86f5af910fc3dcd33573
Andreas Herrmann <aherrmann@suse.com>
2020-09-10 10:25:24 +0200

    run-mmtests.sh: Support to set CGROUP_BLKIO_BFQ_WEIGHT
    Signed-off-by: Andreas Herrmann <aherrmann@suse.com>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- run-mmtests.sh
+++ run-mmtests.sh
@@ -632,12 +632,17 @@ for (( MMTEST_ITERATION = 0; MMTEST_ITERATION < $MMTEST_ITERATIONS; MMTEST_ITERA
 			echo Memory limit configured: `cat /sys/fs/cgroup/memory/0/memory.limit_in_bytes`
 			CGROUP_TASKS[0]=/sys/fs/cgroup/memory/0/tasks
 		fi
-
 		if [ "$CGROUP_CPU_TAG" != "" ]; then
 			mkdir -p /sys/fs/cgroup/cpu/0 || die "Failed to create cpu cgroup"
 			echo $CGROUP_CPU_TAG > /sys/fs/cgroup/cpu/0/cpu.tag || die "Failed to create CPU sched tag"
 			echo CPU Tags set: `cat /sys/fs/cgroup/cpu/0/cpu.tag`
 			CGROUP_TASKS[1]=/sys/fs/cgroup/cpu/0/tasks
+		fi
+		if [ "$CGROUP_BLKIO_BFQ_WEIGHT" != "" ]; then
+			mkdir -p /sys/fs/cgroup/blkio/0 || die "Failed to create blkio cgroup"
+			echo $CGROUP_BLKIO_BFQ_WEIGHT > /sys/fs/cgroup/blkio/0/blkio.bfq.weight || die "Failed to set blkio BFQ weight"
+			echo BLKIO BFQ weight set: `cat /sys/fs/cgroup/blkio/0/blkio.bfq.weight`
+			CGROUP_TASKS[2]=/sys/fs/cgroup/blkio/0/tasks
 		fi
 		declare -p | grep "\-ax" > $SCRIPTDIR/bash_arrays
 
