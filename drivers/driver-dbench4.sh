e7507e389130dba15d37b261c08fe046763dc9a0
Mel Gorman <mgorman@techsingularity.net>
2020-10-17 16:52:20 +0100

    tbench/dbench: Always use git commits as versions are not tagged
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-dbench4.sh
+++ drivers/driver-dbench4.sh
@@ -1,6 +1,6 @@
 $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh dbench
 
 run_bench() {
-	$SHELLPACK_INCLUDE/shellpack-bench-dbench -v 4.1alpha
+	$SHELLPACK_INCLUDE/shellpack-bench-dbench -v 65b19870
 	return $?
 }
