2e0b7b9c0d9a3446ccd679ba7568d2a55771e137
Mel Gorman <mgorman@techsingularity.net>
2021-01-13 11:26:44 +0000

    sysbenchcpu: Allow min threads to be specified
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-sysbenchcpu.sh
+++ drivers/driver-sysbenchcpu.sh
@@ -2,6 +2,7 @@
 run_bench() {
 	eval $SHELLPACK_INCLUDE/shellpack-bench-sysbenchcpu \
 		--max-prime   $SYSBENCHCPU_MAX_PRIME		\
+		--min-threads $SYSBENCHCPU_MIN_THREADS		\
 		--max-threads $SYSBENCHCPU_MAX_THREADS		\
 		--iterations  $SYSBENCHCPU_ITERATIONS
 	RETVAL=$?
