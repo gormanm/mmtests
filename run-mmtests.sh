c32e020dfcd12ac9ddb7d4f60591fb43438e3426
Dario Faggioli <dfaggioli@suse.com>
2021-01-22 09:24:02 +0000

    install-depends: use command, as which itself may be missing
    Seen this in a JeOS image. `which` is not there, so we need to install
    it. But we use `which` to check what package manager to use, and the
    whole thing fails quite spectacularly!
    
    Use the `command` bash builtin for checking the package manager instead.
    And add `which` as a package that needs to be there/needs to be
    installed.
    
    Signed-off-by: Dario Faggioli <dfaggioli@suse.com>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- run-mmtests.sh
+++ run-mmtests.sh
@@ -204,7 +204,7 @@ fi
 install-depends autoconf automake bc binutils-devel btrfsprogs bzip2	\
 	cpupower e2fsprogs expect expect-devel gcc hdparm hwloc libtool	\
 	make numactl patch perl-Time-HiRes psmisc tcl time util-linux	\
-	wget xfsprogs xfsprogs-devel xz
+	wget xfsprogs xfsprogs-devel xz which
 
 # Set some basic performance cpu frequency settings.
 if ! $BUILDONLY && [ "$FORCE_PERFORMANCE_SETUP" = "yes" ]; then
