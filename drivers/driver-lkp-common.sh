85eb8505a529498e8686ef3914577e6e019625f8
Barry Song <song.bao.hua@hisilicon.com>
2021-06-15 12:41:07 +0100

    lkp: correct miin-threads to min-threads
    Installing test lkp-pbzip2
    Unrecognised option: --miin-threads
    Unrecognised option: 2
    lkp installed only as requested.
    lkp-pbzip2 fine
    
    Signed-off-by: Barry Song <song.bao.hua@hisilicon.com>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-lkp-common.sh
+++ drivers/driver-lkp-common.sh
@@ -4,7 +4,7 @@ run_bench() {
 
 	$SHELLPACK_INCLUDE/shellpack-bench-lkp 		\
 		--workload $LKP_WORKLOAD		\
-		--miin-threads $LKP_MIN_THREADS		\
+		--min-threads $LKP_MIN_THREADS		\
 		--max-threads $LKP_MAX_THREADS		\
 		--duration $LKP_DURATION		\
 		--iterations $LKP_ITERATIONS
