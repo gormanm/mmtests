f9b08c75079d849b23f0d70802149449fa46196f
Mel Gorman <mgorman@techsingularity.net>
2021-09-30 12:42:40 +0100

    kernbench: Clarify that the warming run is part of the installation
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-multi.sh
+++ drivers/driver-multi.sh
@@ -1,7 +1,6 @@
-SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh netperf
-SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh kernbench
-SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh tbench4
-
+$SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh netperf
+$SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh kernbench
+$SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh tbench4
 
 run_bench() {
 	$SHELLPACK_INCLUDE/shellpack-bench-multi
