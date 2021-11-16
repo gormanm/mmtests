81dd3cfa6b6a1a159cc186034865b982a33a7685
Jiri Wiesner <jwiesner@suse.de>
2021-11-16 11:12:45 +0000

    profiling: Use here documents for the perf hoo
    Converting the perf hook to use here documents will have there advantages
    * it will make adding commands to the hook with sed easier
    * it will make the code less cluttered
    The string "-report", which seemed redundant, was removed from the base
    name of the data files as well as text files.
    
    Signed-off-by: Jiri Wiesner <jwiesner@suse.de>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- profile-disabled-hooks-perf.sh
+++ profile-disabled-hooks-perf.sh
@@ -5,28 +5,33 @@ export PROFILE_EVENTS=timer
 PERF_RECORD_COMMAND="record -a"
 PERF_REPORT_COMMAND="report"
 
-echo "#!/bin/bash" > monitor-pre-hook
-echo "
-perf $PERF_RECORD_COMMAND  -o \$1/perf-\$2-report-${PROFILE_TITLE}.data &
-echo \$! > /tmp/mmtests.perf.pid
-" >> monitor-pre-hook
+cat << EOF > monitor-pre-hook
+#!/bin/bash
 
-echo "#!/bin/bash" > monitor-post-hook
-echo 'WAITPID=`cat /tmp/mmtests.perf.pid`' >> monitor-post-hook
-echo 'kill $WAITPID' >> monitor-post-hook
-echo 'sleep 1' >> monitor-post-hook
-echo 'echo Waiting on perf pid $WAITPID to exit: `date`' >> monitor-post-hook
-echo 'while [ "`ps h --pid $WAITPID`" != "" ]; do' >> monitor-post-hook
-echo 'echo -n .' >> monitor-post-hook
-echo 'sleep 1' >> monitor-post-hook
-echo 'done' >> monitor-post-hook
-echo 'echo Perf exited: `date`' >> monitor-post-hook
-echo 'echo Creating perf archive' >> monitor-post-hook
-echo "perf archive \$1/perf-\$2-report-${PROFILE_TITLE}.data" >> monitor-post-hook
-echo 'echo Creating perf report' >> monitor-post-hook
-echo "perf $PERF_REPORT_COMMAND -i \$1/perf-\$2-report-${PROFILE_TITLE}.data > \$1/perf-\$2-report-${PROFILE_TITLE}.txt" >> monitor-post-hook
-echo "gzip \$1/perf-\$2-report-${PROFILE_TITLE}.data" >> monitor-post-hook
-echo "exit 0" >> monitor-post-hook
+perf $PERF_RECORD_COMMAND -o \$1/perf-\$2-${PROFILE_TITLE}.data &
+echo \$! > /tmp/mmtests.perf.pid
+EOF
+
+cat << EOF > monitor-post-hook
+#!/bin/bash
+
+WAITPID=\`cat /tmp/mmtests.perf.pid\`
+kill \$WAITPID
+sleep 1
+echo Waiting on perf pid \$WAITPID to exit: \`date\`
+while [ "\`ps h --pid \$WAITPID\`" != "" ]; do
+	echo -n .
+	sleep 1
+done
+echo Perf exited: \`date\`
+
+echo Creating perf archive
+perf archive \$1/perf-\$2-${PROFILE_TITLE}.data
+echo Creating perf report
+perf $PERF_REPORT_COMMAND -i \$1/perf-\$2-${PROFILE_TITLE}.data > \$1/perf-\$2-${PROFILE_TITLE}.txt
+gzip \$1/perf-\$2-${PROFILE_TITLE}.data
+exit 0
+EOF
 
 echo "#!/bin/bash" > monitor-cleanup-hook
 
