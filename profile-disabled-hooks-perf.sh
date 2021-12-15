d6832d4337be6581a189e23ce3b97f6c43144dca
Mel Gorman <mgorman@techsingularity.net>
2021-12-15 10:06:42 +0000

    profile-hooks: Create one symbols archive by default
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- profile-disabled-hooks-perf.sh
+++ profile-disabled-hooks-perf.sh
@@ -4,6 +4,7 @@ export PROFILE_EVENTS=timer
 
 PERF_RECORD_COMMAND="record -a"
 PERF_REPORT_COMMAND="report"
+ARCHIVE_MINIMUM=yes
 
 cat << EOF > monitor-pre-hook
 #!/bin/bash
@@ -15,6 +16,7 @@ EOF
 cat << EOF > monitor-post-hook
 #!/bin/bash
 
+ARCHIVE_ONE=$ARCHIVE_MINIMUM
 WAITPID=\`cat /tmp/mmtests.perf.pid\`
 kill \$WAITPID
 sleep 1
@@ -25,8 +27,21 @@ while [ "\`ps h --pid \$WAITPID\`" != "" ]; do
 done
 echo Perf exited: \`date\`
 
-echo Creating perf archive
-perf archive \$1/perf-\$2-${PROFILE_TITLE}.data
+# Note, this saves space but if there are multiple tests (e.g. many clients)
+# then it assumes that the same binaries are used for each test.
+if [ "\$ARCHIVE_ONE" = "yes" ]; then
+	if [ ! -e \$1/perf-symbol-archive.data.tar.bz2 ]; then
+		echo Creating perf archive
+		perf archive \$1/perf-\$2-${PROFILE_TITLE}.data
+		mv \$1/perf-\$2-${PROFILE_TITLE}.data.tar.bz2 \$1/perf-symbol-archive.data.tar.bz2
+	else
+		echo Archive perf-symbol-archive.data.tar.bz2 already exists, use ARCHIVE_MINIMUM=no to override
+	fi
+else
+	echo Creating perf archive
+	perf archive \$1/perf-\$2-${PROFILE_TITLE}.data
+fi
+
 echo Creating perf report
 perf $PERF_REPORT_COMMAND -i \$1/perf-\$2-${PROFILE_TITLE}.data > \$1/perf-\$2-${PROFILE_TITLE}.txt
 gzip \$1/perf-\$2-${PROFILE_TITLE}.data
