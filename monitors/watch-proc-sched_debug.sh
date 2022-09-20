0e24b885910a5b95dea48b56dcc5a48b32e21a06
Mel Gorman <mgorman@techsingularity.net>
2022-09-20 13:57:43 +0100

    proc-sched_debug: Update path location for newer kernels
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- monitors/watch-proc-sched_debug.sh
+++ monitors/watch-proc-sched_debug.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+SCHED_DEBUG="/proc/sched_debug"
+if [ -e $SCHED_DEBUG ]; then
+	SCHED_DEBUG="/sys/kernel/debug/sched/debug"
+fi
 while [ 1 ]; do
 	echo time: `date +%s`
-	cat /proc/sched_debug
+	cat $SCHED_DEBUG
 	sleep $MONITOR_UPDATE_FREQUENCY
 done
