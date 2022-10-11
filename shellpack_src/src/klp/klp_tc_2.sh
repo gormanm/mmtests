88c31632a43e83e3c7563c09a5a9c4951583ba6c
Mel Gorman <mgorman@techsingularity.net>
2022-10-11 10:17:36 +0100

    klp: Move cyclictest to a common installation and allow patching of getppid
    Patching getppid allows use of perf bench syscall to measure syscall
    overhead after live patching of a basic function.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

---
+++ shellpack_src/src/klp/klp_tc_2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright (C) 2017 SUSE
+# Author: Libor Pechacek (derived from klp_tc_3 with minor modification)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# Test Case 2: Basic patch/unpatch of getppid
+
+set -e
+. $(dirname $0)/klp_tc_functions.sh
+klp_tc_init "Test Case 2: Basic patch/unpatch of getppid"
+
+klp_tc_milestone "Compiling kernel live patch"
+PATCH_KO="$(klp_create_patch_module tc_2 ${KLP_TEST_SYSCALL_FN_PREFIX}sys_getppid)"
+PATCH_MOD_NAME="$(basename "$PATCH_KO" .ko)"
+
+klp_tc_milestone "Inserting getppid patch"
+insmod "$PATCH_KO"
+if [ ! -e /sys/kernel/livepatch/"$PATCH_MOD_NAME" ]; then
+   klp_tc_abort "don't see $PATCH_MOD_NAME in live patch sys directory"
+fi
+register_mod_for_unload "$PATCH_MOD_NAME"
+
+klp_tc_milestone "Wait for completion"
+if ! klp_wait_complete "$PATCH_MOD_NAME" 61; then
+    klp_dump_blocking_processes
+    klp_tc_abort "patching didn't finish in time"
+fi
+
+# test passed if execution reached this line
+# failures beyond this point are not test case failures
+klp_tc_milestone "Terminating"
+
+klp_tc_exit
