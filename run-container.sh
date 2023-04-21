b5858775d6464bf85b210d95016ef23c90718170
Andreas Herrmann <aherrmann@suse.de>
2023-04-21 15:29:00 +0200

    run-container: Fix installation on Ubuntu and Fedora images
    Signed-off-by: Andreas Herrmann <aherrmann@suse.de>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- run-container.sh
+++ run-container.sh
@@ -197,6 +197,10 @@ function prepare_mmtests() {
 	${cli} cp ${SCRIPTDIR} ${container_id}:${c_mmtests_dir}
 	${cli} exec -w ${c_mmtests_dir} ${container_id} \
 	       sed -i -e 's/zypper install/zypper install -y/' bin/install-depends
+	${cli} exec -w ${c_mmtests_dir} ${container_id} \
+	       sed -i -e 's/apt-get install/DEBIAN_FRONTEND=noninteractive apt-get -y install/' bin/install-depends
+	${cli} exec -w ${c_mmtests_dir} ${container_id} \
+	       sed -i -e 's/yum install/yum -y install/' bin/install-depends
 	${cli} exec -w /root ${container_id} \
 	       bash -c "echo -e \"[http]\n\tsslVerify = false\n\" > .gitconfig"
 	${cli} exec -w ${c_mmtests_dir} ${container_id} \
