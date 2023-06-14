c85f212e1b6ae56ef23a41bf34dcab19cb445529
Andreas Herrmann <aherrmann@suse.de>
2023-06-14 16:01:42 +0200

    gitsource: Add option to skip warmup run
    Use 'export GITSOURCE_SKIP_WARMUP=yes'
    in config file to skip it.
    
    Signed-off-by: Andreas Herrmann <aherrmann@suse.de>

--- drivers/driver-gitsource.sh
+++ drivers/driver-gitsource.sh
@@ -1,6 +1,10 @@
 
 run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-gitsource \
+	SKIP_SWITCH=
+	if [ "$GITSOURCE_SKIP_WARMUP" = "yes" ]; then
+		SKIP_SWITCH="--skip-warmup"
+	fi
+	$SCRIPTDIR/shellpacks/shellpack-bench-gitsource $SKIP_SWITCH \
 		--iterations $GITSOURCE_ITERATIONS
 
 	return $?
