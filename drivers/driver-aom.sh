f484de61038094b3aeb2a10a54a83598fd6478a2
Andreas Herrmann <aherrmann@suse.de>
2023-08-01 11:15:10 +0200

    aom: Set aom encoder speed ('--cpu-used')
    Use 5 as default value -- different from 0 (which is default if
    '--cpu-used' is not specified). 0 is unreasonably slow and should be
    avoided.
    
    Signed-off-by: Andreas Herrmann <aherrmann@suse.de>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-aom.sh
+++ drivers/driver-aom.sh
@@ -1,6 +1,11 @@
 run_bench() {
-	$SHELLPACK_INCLUDE/shellpack-bench-aom		\
+	ENCODER_SWITCH=
+	if [ "$AOM_ENCODER_SPEED" != "" ]; then
+		ENCODER_SWITCH="--encoder-speed $AOM_ENCODER_SPEED"
+	fi
+	$SHELLPACK_INCLUDE/shellpack-bench-aom $ENCODER_SWITCH \
 		--min-threads	$AOM_MIN_THREADS	\
-		--max-threads	$AOM_MAX_THREADS
+		--max-threads	$AOM_MAX_THREADS	\
+		--source-file	$AOM_SOURCE_FILE
 	return $?
 }
