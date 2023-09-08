9abe664bf44991b3a28fd71b753f3efc7dda88b6
Jiri Wiesner <jwiesner@suse.de>
2023-09-08 10:30:22 +0200

    profile-hooks-perf: generate many reports by default
    The perf post hook has been reworked to detect the common types of perf
    data files - a stat file, a profile and a profile with call graphs.
    Detailed reports are generated for each of the common types of perf data
    files. Stat data file reports contain total counts and per CPU counts.
    Profile reports are generated for DSOs, DSOs-symbols and
    DSOs-symbols-CPUs. Call graph reports are generated in different variants,
    all in extra detail, and also without call graphs to get an approximate
    profile. The rationale for generating multiple reports is that they cover
    the basic questions one needs to answer and the reports tend to be smaller
    than the original data files (hence better for archiving). Optionally, the
    perf report command may be specified to disable the creation of default
    reports.
    
    Signed-off-by: Jiri Wiesner <jwiesner@suse.de>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- profile-disabled-hooks-perf.sh
+++ profile-disabled-hooks-perf.sh
@@ -1,9 +1,7 @@
 # Create profiling hooks
-PROFILE_TITLE="timer"
-export PROFILE_EVENTS=timer
-
+PROFILE_TITLE=cycles
 PERF_RECORD_COMMAND="record -a"
-PERF_REPORT_COMMAND="report"
+PERF_REPORT_COMMAND=
 ARCHIVE_MINIMUM=yes
 
 cat << EOF > monitor-pre-hook
@@ -16,9 +14,8 @@ EOF
 cat << EOF > monitor-post-hook
 #!/bin/bash
 
-ARCHIVE_ONE=$ARCHIVE_MINIMUM
 WAITPID=\`cat /tmp/mmtests.perf.pid\`
-kill \$WAITPID
+kill -2 \$WAITPID
 sleep 1
 echo Waiting on perf pid \$WAITPID to exit: \`date\`
 while [ "\`ps h --pid \$WAITPID\`" != "" ]; do
@@ -27,24 +24,59 @@ while [ "\`ps h --pid \$WAITPID\`" != "" ]; do
 done
 echo Perf exited: \`date\`
 
-# Note, this saves space but if there are multiple tests (e.g. many clients)
-# then it assumes that the same binaries are used for each test.
-if [ "\$ARCHIVE_ONE" = "yes" ]; then
-	if [ ! -e \$1/perf-symbol-archive.data.tar.bz2 ]; then
-		echo Creating perf archive
-		perf archive \$1/perf-\$2-${PROFILE_TITLE}.data
-		mv \$1/perf-\$2-${PROFILE_TITLE}.data.tar.bz2 \$1/perf-symbol-archive.data.tar.bz2
+ARCHIVE_ONE=$ARCHIVE_MINIMUM
+PROFILE_NAME=perf-\$2-${PROFILE_TITLE}
+perf report --stdio --header-only -i \$1/\$PROFILE_NAME.data | grep -q 'contains stat data'
+IS_STAT=\$?
+perf report --stdio --header-only -i \$1/\$PROFILE_NAME.data | grep -q 'sample_type = .*CALLCHAIN'
+IS_CALLGRAPH=\$?
+
+if [ \$IS_STAT -ne 0 ]; then
+	# Note, this saves space but if there are multiple tests (e.g. many clients)
+	# then it assumes that the same binaries are used for each test.
+	if [ "\$ARCHIVE_ONE" = "yes" ]; then
+		if [ ! -e \$1/perf-symbol-archive.data.tar.bz2 ]; then
+			echo Creating perf archive
+			perf archive \$1/\$PROFILE_NAME.data
+			mv \$1/\$PROFILE_NAME.data.tar.bz2 \$1/perf-symbol-archive.data.tar.bz2
+		else
+			echo Archive perf-symbol-archive.data.tar.bz2 already exists, use ARCHIVE_MINIMUM=no to override
+		fi
 	else
-		echo Archive perf-symbol-archive.data.tar.bz2 already exists, use ARCHIVE_MINIMUM=no to override
+		echo Creating perf archive
+		perf archive \$1/\$PROFILE_NAME.data
 	fi
-else
-	echo Creating perf archive
-	perf archive \$1/perf-\$2-${PROFILE_TITLE}.data
 fi
 
 echo Creating perf report
-perf $PERF_REPORT_COMMAND -i \$1/perf-\$2-${PROFILE_TITLE}.data > \$1/perf-\$2-${PROFILE_TITLE}.txt
-gzip \$1/perf-\$2-${PROFILE_TITLE}.data
+if [ -n "$PERF_REPORT_COMMAND" ]; then
+	perf $PERF_REPORT_COMMAND -i \$1/\$PROFILE_NAME.data > \$1/\$PROFILE_NAME.txt
+elif [ \$IS_STAT -eq 0 ]; then
+	perf stat report \\
+		-i \$1/\$PROFILE_NAME.data &> \$1/\$PROFILE_NAME.txt
+	perf stat report -A \\
+		-i \$1/\$PROFILE_NAME.data &>> \$1/\$PROFILE_NAME.txt
+elif [ \$IS_CALLGRAPH -eq 0 ]; then
+	perf report --stdio -F overhead,sample,period,comm,dso,sym -g none \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\$PROFILE_NAME.txt
+	perf report --stdio --header -F overhead,sample,period,comm,dso -g none \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\${PROFILE_NAME}-dso.txt
+	perf report --stdio -s sample,period,comm,dso,sym -g graph,0.01 \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\${PROFILE_NAME}-cg.txt
+	perf report --stdio -s sample,period,comm -g graph,0.01 \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\${PROFILE_NAME}-cg-comm.txt
+	perf report --stdio -s sample,period,comm,dso,sym -g graph,0.05 --no-children \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\${PROFILE_NAME}-no-children.txt
+else
+	perf report --stdio -s sample,period,comm,dso,sym \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\$PROFILE_NAME.txt
+	perf report --stdio -s sample,period,comm,dso,sym,cpu \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\${PROFILE_NAME}-cpu.txt
+	perf report --header --stdio -s sample,period,comm,dso \\
+		-i \$1/\$PROFILE_NAME.data > \$1/\${PROFILE_NAME}-dso.txt
+fi
+
+gzip \$1/\$PROFILE_NAME.data \$1/\${PROFILE_NAME}*.txt
 exit 0
 EOF
 
