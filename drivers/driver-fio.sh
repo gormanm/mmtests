8c3183271442800029f387e7610134f162ba89ab
Gabriel Krisman Bertazi <krisman@suse.de>
2023-10-24 18:03:25 -0400

    fio_bench: merge block and jobfile handling
    There is nothing special about block devices, other than the fact that
    we commonly use a fixed file target for them instead of letting fio
    decide on a file name.  Provide a script option to specify the target
    instead and rely on that, such that filesystem tests sharing a file can
    also use the filename argument.
    
    Signed-off-by: Gabriel Krisman Bertazi <krisman@suse.de>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-fio.sh
+++ drivers/driver-fio.sh
@@ -1,6 +1,13 @@
 
 run_bench() {
+	# If no filesystem was mounted and there is not a custom fixed file, do
+	# IO on the TEST_PARTITION itself (block device).
+	if [ "${FIO_TARGET_FILE}" = "" -a "${TESTDISK_NOMOUNT}" = "true" ]; then
+		FIO_TARGET_FILE=${TESTDISK_PARTITION}
+	fi
+
 	$SCRIPTDIR/shellpacks/shellpack-bench-fio \
+		${FIO_TARGET_FILE:+--target_file "$FIO_TARGET_FILE"} \
 		${FIO_CMD_OPTIONS:+--cmdline "$FIO_CMD_OPTIONS"}
 	return $?
 }
