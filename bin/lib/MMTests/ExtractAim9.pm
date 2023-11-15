ce11acf2b297e37d27706b727b81529ecb3f7ad1
Mel Gorman <mgorman@techsingularity.net>
2023-11-15 12:01:25 +0000

    aim9: Only report on executed workloads
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/lib/MMTests/ExtractAim9.pm
+++ bin/lib/MMTests/ExtractAim9.pm
@@ -4,24 +4,26 @@ use MMTests::SummariseMultiops;
 use MMTests::Stat;
 our @ISA = qw(MMTests::SummariseMultiops);
 use strict;
+use List::MoreUtils qw(uniq);
 
 sub initialise() {
 	my ($self, $subHeading) = @_;
 	$self->{_ModuleName} = "ExtractAim9";
 	$self->{_DataType}   = DataTypes::DATA_OPS_PER_SECOND;
 	$self->{_PlotType}   = "operation-candlesticks";
-	$self->{_Operations} = [ "page_test", "brk_test", "exec_test", "fork_test", "disk_cp", "disk_rd", "disk_rr", "disk_rw", "disk_src", "disk_wrt" ];
 
 	$self->SUPER::initialise($subHeading);
 }
 
 sub extractReport() {
 	my ($self, $reportDir) = @_;
+	my @operations_parse = ("page_test", "brk_test", "exec_test", "fork_test", "disk_cp", "disk_rd", "disk_rr", "disk_rw", "disk_src", "disk_wrt");
+	my %operations_seen;
 
 	# Multiple reads of the same file, don't really care as this is hardly
 	# performance critical code.
 	my @files = <$reportDir/aim9-*>;
-	foreach my $workload (@{$self->{_Operations}}) {
+	foreach my $workload (@operations_parse) {
 		my $iteration = 0;
 		foreach my $file (@files) {
 			my $input = $self->SUPER::open_log($file);
@@ -29,11 +31,19 @@ sub extractReport() {
 				if ($_ =~ /$workload/) {
 					my @elements = split(/\s+/, $_);
 					$self->addData($workload, ++$iteration, $elements[6]);
+					$operations_seen{$workload} = 1;
 				}
 			}
 			close($input);
 		}
 	}
+
+	my @operations;
+	foreach my $workload (sort keys %operations_seen) {
+		push @operations, $workload;
+	}
+
+	$self->{_Operations} = \@operations;
 }
 
 1;
