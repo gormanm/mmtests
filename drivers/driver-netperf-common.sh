2812f05ab85bcda69b60bbdbed4059a39480ec77
Mel Gorman <mgorman@techsingularity.net>
2023-11-29 15:10:18 +0000

    netperf: Fix testing of unix protocol
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-netperf-common.sh
+++ drivers/driver-netperf-common.sh
@@ -3,31 +3,15 @@ SERVER_SIDE_SUPPORT=yes
 SERVER_SIDE_BENCH_SCRIPT=shellpacks/shellpack-bench-netperf
 
 run_bench() {
-	if [ "$NETPERF_NR_PAIRS" = "" ]; then
-		NETPERF_NR_PAIRS=1
-	fi
-	BIND_SWITCH=
-	VERSION_SWITCH=
-	if [ "$NETPERF_VERSION" != "" ]; then
-		VERSION_SWITCH="-v $NETPERF_VERSION"
-	fi
-
-	if [ "$NETPERF_BINDING" != "" ]; then
-		BIND_SWITCH=--bind-$NETPERF_BINDING
-	fi
-	if [ "$NETPERF_PROTOCOL" = "" ]; then
-		NETPERF_PROTOCOL=UDP_STREAM
-	fi
-	if [ "$NETPERF_SERVER" != "" ]; then
-		SERVER_ADDRESS="--server-address $NETPERF_SERVER"
-	fi
 	$SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh netperf
-	$SCRIPTDIR/shellpacks/shellpack-bench-netperf $BIND_SWITCH $VERSION_SWITCH \
-		$SERVER_ADDRESS \
-		--iterations $NETPERF_ITERATIONS	\
-		--net-protocol $NETPERF_NET_PROTOCOL	\
-		--protocol $NETPERF_PROTOCOL		\
-		--buffer-sizes $NETPERF_BUFFER_SIZES	\
-		--nr-pairs $NETPERF_NR_PAIRS
+	$SCRIPTDIR/shellpacks/shellpack-bench-netperf 					\
+		${NETPERF_VERSION:+-v "${NETPERF_VERSION}"}				\
+		${NETPERF_BINDING:+--bind-"${NETPERF_BINDING}"}				\
+		${NETPERF_SERVER:+--server-address "${NETPERF_SERVER}"}			\
+		${NETPERF_ITERATIONS:+--iterations "${NETPERF_ITERATIONS}"}		\
+		${NETPERF_NR_PAIRS:+--nr-pairs "${NETPERF_NR_PAIRS}"}			\
+		${NETPERF_NET_PROTOCOL:+--net-protocol "${NETPERF_NET_PROTOCOL}"}	\
+		${NETPERF_PROTOCOL:+--protocol "${NETPERF_PROTOCOL}"}			\
+		${NETPERF_BUFFER_SIZES:+--buffer-sizes "${NETPERF_BUFFER_SIZES}"}
 	return $?
 }
