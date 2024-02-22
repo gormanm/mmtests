8a786c77962bbb21d3fdb0ca866d825170486c0d
Andreas Herrmann <aherrmann@suse.de>
2024-02-22 17:07:48 +0100

    run-container: Support removal of FIPS support also for SLE15 BCI
    So far this was only supported for Leap images.
    Thus setting of CONTAINER_NO_FIPS=yes had no effect when a SLE15
    container image was in use.
    
    Signed-off-by: Andreas Herrmann <aherrmann@suse.de>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- run-container.sh
+++ run-container.sh
@@ -264,7 +264,7 @@ function update_container() {
 		${cli} exec ${container_id} yum -y install perl
 	elif $(echo ${image} | grep -q tumbleweed); then
 		${cli} exec ${container_id} zypper install -y perl
-	elif $(echo ${image} | grep -q leap); then
+	elif $(echo ${image} | grep -q "leap\|sle15"); then
 		if [ "${CONTAINER_NO_FIPS}" = "yes" ]; then
 			${cli} exec ${container_id} zypper remove -y \
 			       --clean-deps patterns-base-fips
