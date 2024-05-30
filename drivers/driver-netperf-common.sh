a79b49e84a7c3e74e75ef3780bd750fa98f2c789
Mel Gorman <mgorman@techsingularity.net>
2024-05-30 15:13:10 +0100

    netperf: Allow confidence options to be specified by the config
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-netperf-common.sh
+++ drivers/driver-netperf-common.sh
@@ -4,14 +4,17 @@ SERVER_SIDE_BENCH_SCRIPT=shellpacks/shellpack-bench-netperf
 
 run_bench() {
 	$SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh netperf
-	$SCRIPTDIR/shellpacks/shellpack-bench-netperf 					\
-		${NETPERF_VERSION:+-v "${NETPERF_VERSION}"}				\
-		${NETPERF_BINDING:+--bind-"${NETPERF_BINDING}"}				\
-		${NETPERF_SERVER:+--server-address "${NETPERF_SERVER}"}			\
-		${NETPERF_ITERATIONS:+--iterations "${NETPERF_ITERATIONS}"}		\
-		${NETPERF_NR_PAIRS:+--nr-pairs "${NETPERF_NR_PAIRS}"}			\
-		${NETPERF_NET_PROTOCOL:+--net-protocol "${NETPERF_NET_PROTOCOL}"}	\
-		${NETPERF_PROTOCOL:+--protocol "${NETPERF_PROTOCOL}"}			\
-		${NETPERF_BUFFER_SIZES:+--buffer-sizes "${NETPERF_BUFFER_SIZES}"}
+	$SCRIPTDIR/shellpacks/shellpack-bench-netperf 						\
+		${NETPERF_VERSION:+-v "${NETPERF_VERSION}"}					\
+		${NETPERF_BINDING:+--bind-"${NETPERF_BINDING}"}					\
+		${NETPERF_SERVER:+--server-address "${NETPERF_SERVER}"}				\
+		${NETPERF_ITERATIONS:+--iterations "${NETPERF_ITERATIONS}"}			\
+		${NETPERF_NR_PAIRS:+--nr-pairs "${NETPERF_NR_PAIRS}"}				\
+		${NETPERF_NET_PROTOCOL:+--net-protocol "${NETPERF_NET_PROTOCOL}"}		\
+		${NETPERF_PROTOCOL:+--protocol "${NETPERF_PROTOCOL}"}				\
+		${NETPERF_BUFFER_SIZES:+--buffer-sizes "${NETPERF_BUFFER_SIZES}"}		\
+		${NETPERF_CONFIDENCE_MIN:+--confidence-min "${NETPERF_CONFIDENCE_MIN}"}		\
+		${NETPERF_CONFIDENCE_MAX:+--confidence-max "${NETPERF_CONFIDENCE_MAX}"}		\
+		${NETPERF_CONFIDENCE_LEVEL:+--confidence-level "${NETPERF_CONFIDENCE_LEVEL}"}
 	return $?
 }
