3f8cfa834a88a0f0548d5b07df3eafcdd8a30634
Mel Gorman <mgorman@techsingularity.net>
2024-09-06 10:42:54 +0100

    ebizzy: Auto-generate configs with one thread count -- single, max and overload
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-ebizzy.sh
+++ drivers/driver-ebizzy.sh
@@ -1,6 +1,7 @@
 
 run_bench() {
 	$SHELLPACK_INCLUDE/shellpack-bench-ebizzy \
+		--min-threads $EBIZZY_MIN_THREADS \
 		--max-threads $EBIZZY_MAX_THREADS \
 		--iterations $EBIZZY_ITERATIONS \
 		--duration $EBIZZY_DURATION
