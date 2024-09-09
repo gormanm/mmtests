3301a75025c55a221c466ba3e4929ff3e787e46a
Kostas Peletidis <kostas.peletidis@suse.com>
2024-09-09 15:30:29 +0200

    bin: Added cpuplot.sh for plotting CPU selections (time series)
    This script parses a file containing the output of 'perf script' and
    it creates a time series plot, using gnuplot, of CPU allocations to
    new tasks.

---
+++ bin/cpuplot.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Script to parse a file containing the output of 'perf script' and
+# create a time series plot of CPU allocations to new tasks.
+#
+# Dependencies: perf, gnuplot
+#
+# Kostas Peletidis, 2024-09-05
+#
+TRACESTR="sched_wakeup_new"
+
+function show_usage() {
+    echo "Usage: $0 perf_script_output_file [data_set_name]"
+}
+
+## Function to generate a gnuplot script
+## Parameters:
+## $1: Name of input data file to plot
+## $2: Name of output image file
+## $3: Title of the plotted data e.g. "Baseline" or "XYZ Beta1"
+function generate_plot_script() {
+    echo "reset
+	  set terminal png nocrop enhanced size 1280,960 font \"arial,12.0\"
+	  set output '"$2"'
+	  set title 'CPU allocations over time'
+	  set xlabel 'Time (s)'
+	  set ylabel 'CPU #'
+	  plot '"$1"' u 1:2 title '"$3"' with points" \
+    | sed -e 's/^[ \t]*//'
+}
+
+if [ $# -eq 0 -o $# -gt 2 ]; then
+    show_usage
+    exit 1
+elif [ ! -f "$1" ]; then
+    echo "File $1 does not exist"
+    exit 2
+fi
+
+UP_TO_THE_DOT=`echo $1 | awk -F '.' '{print $1}'`
+DATAFILE="${UP_TO_THE_DOT}_cpu_data"
+OUTFILE="${UP_TO_THE_DOT}_cpu_ts.png"
+GNUPLOTFILE="${UP_TO_THE_DOT}_cpu_ts.gnuplot"
+DATANAME="$2"
+
+grep "$TRACESTR" $1 | tr : \  | awk '{print $4" "$11}' > "$DATAFILE"
+
+generate_plot_script "$DATAFILE" "$OUTFILE" "$DATANAME" > "$GNUPLOTFILE"
+gnuplot "$GNUPLOTFILE"
+
+exit 0
+
