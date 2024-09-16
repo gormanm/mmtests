5a1f4ab15ed6df35b7e3761f0480711c8c0322ff
Mel Gorman <mgorman@techsingularity.net>
2024-09-16 15:25:05 +0100

    cyclictest: Allow nr threads to be configured
    If not specified, --threads on its own uses all available CPUs. Allow a
    specific number to be configured.
    
    igned-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-cyclictest.sh
+++ drivers/driver-cyclictest.sh
@@ -2,12 +2,11 @@ $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh cyclictest
 
 run_bench() {
 	$SHELLPACK_INCLUDE/shellpack-bench-cyclictest				\
-		${CYCLICTEST_PINNED:+--affinity}				\
+		${CYCLICTEST_AFFINITY_ALL:+--affinity-all}			\
 		${CYCLICTEST_BACKGROUND:+--background $CYCLICTEST_BACKGROUND}	\
 		${CYCLICTEST_PRIORITY:+--priority $CYCLICTEST_PRIORITY}		\
 		${CYCLICTEST_DISTANCE:+--distance $CYCLICTEST_DISTANCE}		\
 		${CYCLICTEST_DURATION:+--duration $CYCLICTEST_DURATION}		\
-		${CYCLICTEST_AFFINITY_ALL:+--affinity-all}			\
 		${CYCLICTEST_FINEGRAINED:+--fine-grained}
 	return $?
 }
