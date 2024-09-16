140f4cf9c9f2f7c420c6a139eaef23667db24b90
Mel Gorman <mgorman@techsingularity.net>
2024-09-16 11:08:36 +0100

    cyclictest: Allow priority of first thread to be specified
    cyclictest automatically assigns lower priority to subsequent threads
    but the existing value of -p99 for the first thread is not generally
    recommended.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-cyclictest-common.sh
+++ drivers/driver-cyclictest-common.sh
@@ -1,14 +1,10 @@
 $SHELLPACK_TOPLEVEL/shellpack_src/src/refresh.sh cyclictest
 
 run_bench() {
-	if [ "$CYCLICTEST_PINNED" = "yes" ]; then
-		CYCLICTEST_PINNED_PARAM="--affinity"
-	fi
-	CYCLICTEST_BACKGROUND_PARAM=
-	if [ "$CYCLICTEST_BACKGROUND" != "" ]; then
-		CYCLICTEST_BACKGROUND_PARAM="--background $CYCLICTEST_BACKGROUND"
-	fi
-	$SHELLPACK_INCLUDE/shellpack-bench-cyclictest $CYCLICTEST_PINNED_PARAM $CYCLICTEST_BACKGROUND_PARAM \
-		--duration   $CYCLICTEST_DURATION
+	$SHELLPACK_INCLUDE/shellpack-bench-cyclictest				\
+		${CYCLICTEST_PINNED:+--affinity}				\
+		${CYCLICTEST_BACKGROUND:+--background $CYCLICTEST_BACKGROUND}	\
+		${CYCLICTEST_PRIORITY:+--priority $CYCLICTEST_PRIORITY}		\
+		${CYCLICTEST_DURATION:+--duration $CYCLICTEST_DURATION}
 	return $?
 }
