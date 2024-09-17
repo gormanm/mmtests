605ec87a7a98362f362d882db4bbba0d854e7e0a
Mel Gorman <mgorman@techsingularity.net>
2024-09-17 11:08:23 +0100

    microtput: Remove
    Original benchmark no longer available and is unused.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-microtput.sh
+++
@@ -1,8 +0,0 @@
-
-run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-microtput \
-		--min-threads $MICROTPUT_MIN_THREADS \
-		--max-threads $MICROTPUT_MAX_THREADS \
-		--iterations  $MICROTPUT_ITERATIONS
-	return $?
-}
