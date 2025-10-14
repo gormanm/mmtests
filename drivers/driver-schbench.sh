ab4180e4e12b286ed14aa98ca126cad297924763
Mel Gorman <mgorman@techsingularity.net>
2025-10-14 12:18:00 +0100

    schbench: Add TTWU stress test
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-schbench.sh
+++ drivers/driver-schbench.sh
@@ -1,10 +1,14 @@
 
 run_bench() {
 	$SCRIPTDIR/shellpacks/shellpack-bench-schbench \
-		--messangers	 $SCHBENCH_MESSAGE_THREADS	\
-		--min-workers	 $SCHBENCH_MIN_WORKER_THREADS	\
-		--max-workers    $SCHBENCH_MAX_WORKER_THREADS	\
-		--interval	 $SCHBENCH_INTERVAL		\
+		${SCHBENCH_DISABLE_SPINLOCK:+--disable-spinlock}				\
+		${SCHBENCH_MESSENGER_PIN:+--messenger-pin "$SCHBENCH_MESSENGER_PIN"}		\
+		${SCHBENCH_THINKTIME_OPS:+--thinktime-ops "$SCHBENCH_THINKTIME_OPS"}		\
+		${SCHBENCH_THINKTIME_SLEEP:+--thinktime-sleep "$SCHBENCH_THINKTIME_SLEEP"}	\
+		--messengers	 $SCHBENCH_MESSAGE_THREADS					\
+		--min-workers	 $SCHBENCH_MIN_WORKER_THREADS					\
+		--max-workers    $SCHBENCH_MAX_WORKER_THREADS					\
+		--interval	 $SCHBENCH_INTERVAL						\
 		--runtime	 $SCHBENCH_RUNTIME
 	return $?
 }
