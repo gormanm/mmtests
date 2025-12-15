9e4bf2c5cf1839eaad2def130179e6ecf4bdb767
Mel Gorman <mgorman@techsingularity.net>
2025-12-15 22:15:01 +0000

    nap: Remove unmaintained and obsolete benchmark
    Reason for existence lost in time.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- drivers/driver-nap.sh
+++
@@ -1,7 +0,0 @@
-run_bench() {
-	$SHELLPACK_INCLUDE/shellpack-bench-nap \
-		${NAP_DURATION:+--duration "${NAP_DURATION}"}		\
-		${NAP_MSG_INTERVAL:+--interval "${NAP_MSG_INTERVAL}"}
-
-	return $?
-}
