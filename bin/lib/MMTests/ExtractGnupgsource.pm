2105cef1b9a168ec8d00b2790b36234b2f571db6
Mel Gorman <mgorman@techsingularity.net>
2025-12-15 15:14:40 +0000

    gnupgsource: Remove unmaintained and obsolete benchmark
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- bin/lib/MMTests/ExtractGnupgsource.pm
+++
@@ -1,6 +0,0 @@
-# ExtractGnupgsource.pm
-package MMTests::ExtractGnupgsource;
-use MMTests::ExtractCputime;
-our @ISA = qw(MMTests::ExtractCputime);
-
-1;
--- drivers/driver-gnupgsource.sh
+++
@@ -1,8 +0,0 @@
-
-run_bench() {
-	$SCRIPTDIR/shellpacks/shellpack-bench-gnupgsource 				\
-		${GNUPGSOURCE_SKIP_WARMUP:+--skip-warmup}				\
-		${GNUPGSOURCE_ITERATIONS:+--iterations "${GNUPGSOURCE_ITERATIONS}"}
-
-	return $?
-}
