6c93ff57a992e0c50ffccf7aced045916fb1e751
Mel Gorman <mgorman@techsingularity.net>
2026-01-05 16:06:45 +0000

    sysbenchthread: Remove unused benchmark
    Value for identifying kernel bugs is somewhat limited.
    
    Signed-off-by: MEl Gorman <mgorman@techsingularity.net>

--- drivers/driver-sysbenchthread.sh
+++
@@ -1,8 +0,0 @@
-
-run_bench() {
-	eval $SHELLPACK_INCLUDE/shellpack-bench-sysbenchthread \
-		--max-threads $SYSBENCHTHREAD_MAX_THREADS	\
-		--iterations  $SYSBENCHTHREAD_ITERATIONS
-	RETVAL=$?
-	return $RETVAL
-}
