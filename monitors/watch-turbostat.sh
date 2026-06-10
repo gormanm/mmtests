6aac63648123230405e109e525b24692ec0cf450
Mel Gorman <mgorman@techsingularity.net>
2026-06-10 21:13:12 +0100

    turbostat: disable use of perf API in later versions
    Unexpected PMU usage can affect results or conflict with any other
    PMU-related profiling that is enabled.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>

--- monitors/watch-turbostat.sh
+++ monitors/watch-turbostat.sh
@@ -2,4 +2,11 @@
 install-depends cpufrequtils
 install-depends cpupower
 modprobe msr
-exec turbostat -i $MONITOR_UPDATE_FREQUENCY
+
+EXTRA_PARAM=
+MAJOR_VERSION=`turbostat --version 2>&1 | awk '{print $3}' | awk -F . '{print $1}'`
+if [ $MAJOR_VERSION -ge 2026 ]; then
+	EXTRA_PARAM="--no-perf"
+fi
+
+exec turbostat $EXTRA_PARAM -i $MONITOR_UPDATE_FREQUENCY
